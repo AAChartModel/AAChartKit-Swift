41c7e271083f3bb2a501378adaf9827e97afed5d
李照闫 <2236368544@qq.com>
2017-04-18 18:43:02 +0800

    Add  a new repository

---
+++ AAChartKit-Swift/AAChartKit-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AAChartKit-Swift
+//
+//  Created by Danny boy on 17/4/18.
+//  Copyright © 2017年 Danny boy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AAChartKit-Swift/AAChartKit-SwiftTests/AAChartKit_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  AAChartKit_SwiftTests.swift
+//  AAChartKit-SwiftTests
+//
+//  Created by Danny boy on 17/4/18.
+//  Copyright © 2017年 Danny boy. All rights reserved.
+//
+
+import XCTest
+@testable import AAChartKit_Swift
+
+class AAChartKit_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
