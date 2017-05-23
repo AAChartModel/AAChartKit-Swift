5ac6a588d95a0de4031f2010a12cf0a2164e8eaa
李照闫 <2236368544@qq.com>
2017-05-23 19:51:39 +0800

    Initial completion of  creating ` AAChartView`

---
+++ AAChartKit-Swift/AAChartKit-Swift/TestChartView.swift
@@ -0,0 +1,48 @@
+//
+//  TestChartView.swift
+//  AAChartKit-Swift
+//
+//  Created by An An on 2017/5/23.
+//  Copyright © 2017年 Danny boy. All rights reserved.
+//
+
+import UIKit
+
+class TestChartView: UIView,UIWebViewDelegate {
+open var globalWebview: UIWebView?
+  open var model: AAChartModel?
+ open var optionsJson: String?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //检查 AAChartView是否存在
+    self.backgroundColor =  UIColor.cyan
+            globalWebview = AAChartView()
+            globalWebview?.delegate=self;
+            self.addSubview(globalWebview!)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func drawChartWithChartModel(_ chartModel: AAChartModel){
+        //检查 AAChartView是否存在
+        if let htmlFile = Bundle.main.path(forResource: "AAChartView", ofType: "html"){
+            let htmlData = NSData(contentsOfFile: htmlFile)
+            let baseURL = NSURL.fileURL(withPath: Bundle.main.bundlePath)
+             globalWebview?.load(htmlData! as Data, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            
+            let modelString = chartModel.toJSON();
+                let jsString = NSString.localizedStringWithFormat("loadTheHighChartView('%@','%f','%f');", modelString!,self.frame.size.width,self.frame.size.height);
+             optionsJson = jsString as String;
+             globalWebview?.frame = CGRect(x:0,y:0,width:self.frame.size.width,height:self.frame.size.height)
+        }
+    }
+    open func webViewDidFinishLoad(_ webView: UIWebView) {
+        globalWebview?.stringByEvaluatingJavaScript(from: optionsJson!);
+    }
+    
+
+}
