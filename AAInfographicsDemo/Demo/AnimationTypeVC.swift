70f8b20d501efd7e70b0174d0fcf8804844c5374
李照闫 <2236368544@qq.com>
2017-10-30 14:56:54 +0800

    Append the `WARM TIPS` content

--- AAInfographicsDemo/Demo/AnimationTypeVC.swift
+++ AAInfographicsDemo/Demo/AnimationTypeVC.swift
@@ -2,9 +2,29 @@
 //  ViewController.swift
 //  AAInfographicsDemo
 //
-//  Created by An An on 2017/6/8.
-//  Copyright © 2017年 An An. All rights reserved.
+//  Created by An An  on 17/4/18.
+//  Copyright © 2017年 An An . All rights reserved.
+//  source code ----*** https://github.com/AAChartModel/AAChartKit-Swift ***--- source code
 //
+//
+
+/*
+ 
+ *********************************************************************************
+ *
+ * ❀❀❀   WARM TIPS!!!   ❀❀❀
+ * Please contact me on GitHub,if there are any problems encountered in use.
+ * GitHub Issues : https://github.com/AAChartModel/AAChartKit-Swift/issues
+ * -------------------------------------------------------------------------------
+ * And if you want to contribute for this project, please contact me as well
+ * GitHub        : https://github.com/AAChartModel
+ * StackOverflow : https://stackoverflow.com/users/7842508/codeforu
+ * JianShu       : http://www.jianshu.com/u/f1e6753d4254
+ * SegmentFault  : https://segmentfault.com/u/huanghunbieguan
+ *
+ *********************************************************************************
+ 
+ */
 
 import UIKit
 
--- AAInfographicsDemoTests/AAInfographicsDemoTests.swift
+++ AAInfographicsDemoTests/AAInfographicsDemoTests.swift
@@ -6,6 +6,24 @@
 //  Copyright © 2017年 An An. All rights reserved.
 //
 
+/*
+ 
+ * -------------------------------------------------------------------------------
+ *
+ * ❀❀❀   WARM TIPS!!!   ❀❀❀
+ * Please contact me on GitHub,if there are any problems encountered in use.
+ * GitHub Issues : https://github.com/AAChartModel/AAChartKit-Swift/issues
+ * -------------------------------------------------------------------------------
+ * And if you want to contribute for this project, please contact me as well
+ * GitHub        : https://github.com/AAChartModel
+ * StackOverflow : https://stackoverflow.com/users/7842508/codeforu
+ * JianShu       : http://www.jianshu.com/u/f1e6753d4254
+ * SegmentFault  : https://segmentfault.com/u/huanghunbieguan
+ *
+ * -------------------------------------------------------------------------------
+ 
+ */
+
 import XCTest
 @testable import AAInfographicsDemo
 
