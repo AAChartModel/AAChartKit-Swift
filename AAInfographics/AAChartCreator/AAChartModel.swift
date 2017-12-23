1f87e8648dec0332bab8c036b49a19262a34244d
An An <2236368544@qq.com>
2017-12-23 19:11:19 +0800

    Remove the default animation type setting

--- AAInfographics/AAChartCreator/AAChartModel.swift
+++ AAInfographics/AAChartCreator/AAChartModel.swift
@@ -354,7 +354,7 @@ public class AAChartModel:AASerializable {
     public  init() {
 //        self.backgroundColor     = "#4b2b7f"
         self.backgroundColor     = "#ffffff"
-        self.animationType       = AAChartAnimationType.EaseInOutQuart.rawValue
+//        self.animationType       = AAChartAnimationType.EaseInOutQuart.rawValue
         self.animationDuration   = 800//以毫秒为单位
         self.chartType           = AAChartType.Column.rawValue
         self.inverted            = false
