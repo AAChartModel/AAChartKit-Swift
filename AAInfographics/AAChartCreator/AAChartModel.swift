a941d718a2cf8fe528fa63adacfe85b24b288f2e
An An <2236368544@qq.com>
2018-10-15 11:38:29 +0800

    Delete redundant `self.` and beautify the code typography

--- AAInfographics/AAChartCreator/AAChartModel.swift
+++ AAInfographics/AAChartCreator/AAChartModel.swift
@@ -30,7 +30,7 @@
  
  */
 
- public enum AAChartAnimationType:String {
+ public enum AAChartAnimationType: String {
     case EaseInQuad     = "easeInQuad"
     case EaseOutQuad    = "easeOutQuad"
     case EaseInOutQuad  = "easeInOutQuad"
@@ -67,7 +67,7 @@
     case EaseTo         = "easeTo"
 }
 
-public enum AAChartType:String {
+public enum AAChartType: String {
     case Column          = "column"
     case Bar             = "bar"
     case Area            = "area"
@@ -87,26 +87,26 @@ public enum AAChartType:String {
     case Polygon         = "polygon"
 }
 
-public enum AAChartSubtitleAlignType:String {
+public enum AAChartSubtitleAlignType: String {
     case Left   = "left"
     case Center = "center"
     case Right  = "right"
 }
 
-public enum AAChartZoomType:String {
+public enum AAChartZoomType: String {
     case None = "none"
     case X    = "x"
     case Y    = "y"
     case XY   = "xy"
 }
 
-public enum AAChartStackingType:String {
+public enum AAChartStackingType: String {
     case False   = ""
     case Normal  = "normal"
     case Percent = "percent"
 }
 
-public enum AAChartSymbolType:String {
+public enum AAChartSymbolType: String {
     case Circle        = "circle"
     case Square        = "square"
     case Diamond       = "diamond"
@@ -114,30 +114,30 @@ public enum AAChartSymbolType:String {
     case Triangle_down = "triangle-down"
 }
 
-public enum AAChartSymbolStyleType:String {
+public enum AAChartSymbolStyleType: String {
     case Normal      = "normal"
     case InnerBlank  = "innerBlank"
     case BorderBlank = "borderBlank"
 }
 
-public enum AAchartLegendlLayoutType:String {
+public enum AAchartLegendlLayoutType: String {
     case Horizontal = "horizontal"
     case Vertical   = "vertical"
 }
 
-public enum AAChartLegendAlignType:String {
+public enum AAChartLegendAlignType: String {
     case Left   = "left"
     case Center = "center"
     case Right  = "right"
 }
 
-public enum AAChartLegendVerticalAlignType:String {
+public enum AAChartLegendVerticalAlignType: String {
     case Top    = "top"
     case Middle = "middle"
     case Bottom = "bottom"
 }
 
-public enum AALineDashSyleType:String {
+public enum AALineDashSyleType: String {
     case Solid           = "Solid"
     case ShortDash       = "ShortDash"
     case ShortDot        = "ShortDot"
@@ -151,322 +151,320 @@ public enum AALineDashSyleType:String {
     case LongDashDotDot  = "LongDashDotDot"
 }
 
-public class AAChartModel:AASerializable {
-    private var animationType:String?       //动画类型
-    private var animationDuration:Int?      //动画时间
-    private var title:String?               //标题内容
-    private var subtitle:String?            //副标题内容
-    private var chartType:String?           //图表类型
-    private var stacking:String?            //堆积样式
-    private var symbol:String?              //折线曲线连接点的类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
-    private var symbolStyle:String?         //折线或者曲线的连接点是否为空心的
-    private var zoomType:String?            //缩放类型 AAChartZoomTypeX表示可沿着 x 轴进行手势缩放
-    private var inverted:Bool?              //x 轴是否翻转(垂直)
-    private var xAxisReversed:Bool?         //x 轴翻转
-    private var yAxisReversed:Bool?         //y 轴翻转
-    private var gradientColorEnable:Bool?   //是否要为渐变色
-    private var polar:Bool?                 //是否极化图形(变为雷达图)
-    private var marginLeft:Float?
-    private var marginRight:Float?
-    private var dataLabelEnabled:Bool?      //是否显示数据
-    private var xAxisLabelsEnabled:Bool?    //x轴是否显示数据
-    private var categories:[String]?        //x轴是否显示数据
-    private var xAxisGridLineWidth:Float?   //x轴网格线的宽度
-    private var xAxisVisible:Bool?          //x轴是否显示
-    private var yAxisVisible:Bool?          //y轴是否显示
-    private var yAxisLabelsEnabled:Bool?    //y轴是否显示数据
-    private var yAxisTitle:String?          //y轴标题
-    private var yAxisLineWidth:Float?       //y轴轴线的宽度
-    private var yAxisGridLineWidth:Float?   //y轴网格线的宽度
-    private var tooltipEnabled:Bool?        //是否显示浮动提示框(默认显示)
-    private var tooltipValueSuffix:String?  //浮动提示框单位后缀
-    private var tooltipCrosshairs:Bool?     //是否显示准星线(默认显示)
-    private var colorsTheme:[Any]?          //图表主题颜色数组
-    private var series:[[String: Any]]?     //图表的数据数组
-    private var legendEnabled:Bool?         //是否显示图例
-    private var legendLayout:String?        //图例数据项的布局。布局类型： "horizontal" 或 "vertical" 即水平布局和垂直布局 默认是：horizontal.
-    private var legendAlign:String?         //设定图例在图表区中的水平对齐方式，合法值有left，center 和 right。
-    private var legendVerticalAlign:String? //设定图例在图表区中的垂直对齐方式，合法值有 top，middle 和 bottom。垂直位置可以通过 y 选项做进一步设定。
-    private var backgroundColor:String?     //图表背景色
-    private var borderRadius:Int?           //柱状图长条图头部圆角半径(可用于设置头部的形状,仅对条形图,柱状图有效)
-    private var markerRadius:Int?           //折线连接点的半径长度
-    private var titleColor:String?
-    private var subTitleColor:String?
-    private var axisColor:String?
+public class AAChartModel: AASerializable {
+    private var animationType: String?       //动画类型
+    private var animationDuration: Int?      //动画时间
+    private var title: String?               //标题内容
+    private var subtitle: String?            //副标题内容
+    private var chartType: String?           //图表类型
+    private var stacking: String?            //堆积样式
+    private var symbol: String?              //折线曲线连接点的类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
+    private var symbolStyle: String?         //折线或者曲线的连接点是否为空心的
+    private var zoomType: String?            //缩放类型 AAChartZoomTypeX表示可沿着 x 轴进行手势缩放
+    private var inverted: Bool?              //x 轴是否翻转(垂直)
+    private var xAxisReversed: Bool?         //x 轴翻转
+    private var yAxisReversed: Bool?         //y 轴翻转
+    private var gradientColorEnable: Bool?   //是否要为渐变色
+    private var polar: Bool?                 //是否极化图形(变为雷达图)
+    private var marginLeft: Float?
+    private var marginRight: Float?
+    private var dataLabelEnabled: Bool?      //是否显示数据
+    private var xAxisLabelsEnabled: Bool?    //x轴是否显示数据
+    private var categories: [String]?        //x轴是否显示数据
+    private var xAxisGridLineWidth: Float?   //x轴网格线的宽度
+    private var xAxisVisible: Bool?          //x轴是否显示
+    private var yAxisVisible: Bool?          //y轴是否显示
+    private var yAxisLabelsEnabled: Bool?    //y轴是否显示数据
+    private var yAxisTitle: String?          //y轴标题
+    private var yAxisLineWidth: Float?       //y轴轴线的宽度
+    private var yAxisGridLineWidth: Float?   //y轴网格线的宽度
+    private var tooltipEnabled: Bool?        //是否显示浮动提示框(默认显示)
+    private var tooltipValueSuffix: String?  //浮动提示框单位后缀
+    private var tooltipCrosshairs: Bool?     //是否显示准星线(默认显示)
+    private var colorsTheme: [Any]?          //图表主题颜色数组
+    private var series: [[String: Any]]?     //图表的数据数组
+    private var legendEnabled: Bool?         //是否显示图例
+    private var legendLayout: String?        //图例数据项的布局。布局类型： "horizontal" 或 "vertical" 即水平布局和垂直布局 默认是：horizontal.
+    private var legendAlign: String?         //设定图例在图表区中的水平对齐方式，合法值有left，center 和 right。
+    private var legendVerticalAlign: String? //设定图例在图表区中的垂直对齐方式，合法值有 top，middle 和 bottom。垂直位置可以通过 y 选项做进一步设定。
+    private var backgroundColor: String?     //图表背景色
+    private var borderRadius: Int?           //柱状图长条图头部圆角半径(可用于设置头部的形状,仅对条形图,柱状图有效)
+    private var markerRadius: Int?           //折线连接点的半径长度
+    private var titleColor: String?
+    private var subTitleColor: String?
+    private var axisColor: String?
     
     @discardableResult
     public func animationType(_ prop: AAChartAnimationType) -> AAChartModel {
-//       Array<Dictionary<String, Any>>
-        
-        self.animationType = prop.rawValue
+        animationType = prop.rawValue
         return self
     }
     
     @discardableResult
     public func animationDuration(_ prop: Int) -> AAChartModel {
-        self.animationDuration = prop
+        animationDuration = prop
         return self
     }
     
     @discardableResult
     public func title(_ prop: String) -> AAChartModel {
-        self.title = prop
+        title = prop
         return self
     }
     
     @discardableResult
     public func subtitle(_ prop: String) -> AAChartModel {
-        self.subtitle = prop
+        subtitle = prop
         return self
     }
     
     @discardableResult
     public func chartType(_ prop: AAChartType) -> AAChartModel {
-        self.chartType = prop.rawValue
+        chartType = prop.rawValue
         return self
     }
     
     @discardableResult
     public func stacking(_ prop: AAChartStackingType) -> AAChartModel {
-        self.stacking = prop.rawValue
+        stacking = prop.rawValue
         return self
     }
     
     @discardableResult
     public func symbol(_ prop: AAChartSymbolType) -> AAChartModel {
-        self.symbol = prop.rawValue
+        symbol = prop.rawValue
         return self
     }
     
     @discardableResult
     public func zoomType(_ prop: AAChartZoomType) -> AAChartModel {
-        self.zoomType = prop.rawValue
+        zoomType = prop.rawValue
         return self
     }
     
     @discardableResult
     public func inverted(_ prop: Bool) -> AAChartModel {
-        self.inverted = prop
+        inverted = prop
         return self
     }
     
     @discardableResult
     public func symbolStyle(_ prop: AAChartSymbolStyleType) -> AAChartModel {
-        self.symbolStyle = prop.rawValue
+        symbolStyle = prop.rawValue
         return self
     }
     
     @discardableResult
     public func xAxisReversed(_ prop: Bool) -> AAChartModel {
-        self.xAxisReversed = prop
+        xAxisReversed = prop
         return self
     }
     
     @discardableResult
     public func yAxisReversed(_ prop: Bool) -> AAChartModel {
-        self.yAxisReversed = prop
+        yAxisReversed = prop
         return self
     }
     
     @discardableResult
-    public func tooltipEnabled(_ prop:Bool) -> AAChartModel {
-        self.tooltipEnabled = prop
+    public func tooltipEnabled(_ prop: Bool) -> AAChartModel {
+        tooltipEnabled = prop
         return self
     }
     
     @discardableResult
-    public func tooltipValueSuffix(_ prop:String) -> AAChartModel {
-        self.tooltipValueSuffix = prop
+    public func tooltipValueSuffix(_ prop: String) -> AAChartModel {
+        tooltipValueSuffix = prop
         return self
     }
     
     @discardableResult
     public func tooltipCrosshairs(_ prop: Bool) -> AAChartModel {
-        self.tooltipCrosshairs = prop
+        tooltipCrosshairs = prop
         return self
     }
     
     @discardableResult
     public func gradientColorEnable(_ prop: Bool) -> AAChartModel {
-        self.gradientColorEnable = prop
+        gradientColorEnable = prop
         return self
     }
     
     @discardableResult
     public func polar(_ prop: Bool) -> AAChartModel {
-        self.polar = prop
+        polar = prop
         return self
     }
     
     @discardableResult
     public func marginLeft(_ prop: Float) -> AAChartModel {
-        self.marginLeft = prop
+        marginLeft = prop
         return self
     }
     
     @discardableResult
     public func marginRight(_ prop: Float) -> AAChartModel {
-        self.marginRight = prop
+        marginRight = prop
         return self
     }
     
     @discardableResult
     public func dataLabelEnabled(_ prop: Bool) -> AAChartModel {
-        self.dataLabelEnabled = prop
+        dataLabelEnabled = prop
         return self
     }
     
     @discardableResult
     public func xAxisLabelsEnabled(_ prop: Bool) -> AAChartModel {
-        self.xAxisLabelsEnabled = prop
+        xAxisLabelsEnabled = prop
         return self
     }
     
     @discardableResult
     public func categories(_ prop: [String]) -> AAChartModel {
-        self.categories = prop
+        categories = prop
         return self
     }
     
     @discardableResult
     public func xAxisGridLineWidth(_ prop: Float) -> AAChartModel {
-        self.xAxisGridLineWidth = prop
+        xAxisGridLineWidth = prop
         return self
     }
     
     @discardableResult
     public func xAxisVisible(_ prop: Bool) -> AAChartModel {
-        self.xAxisVisible = prop
+        xAxisVisible = prop
         return self
     }
     
     @discardableResult
     public func yAxisVisible(_ prop: Bool) -> AAChartModel {
-        self.yAxisVisible = prop
+        yAxisVisible = prop
         return self
     }
     
     @discardableResult
     public func yAxisLabelsEnabled(_ prop: Bool) -> AAChartModel {
-        self.yAxisLabelsEnabled = prop
+        yAxisLabelsEnabled = prop
         return self
     }
     
     @discardableResult
     public func yAxisTitle(_ prop: String) -> AAChartModel {
-        self.yAxisTitle = prop
+        yAxisTitle = prop
         return self
     }
     
     @discardableResult
     public func yAxisGridLineWidth(_ prop: Float) -> AAChartModel {
-        self.yAxisGridLineWidth = prop
+        yAxisGridLineWidth = prop
         return self
     }
     
     @discardableResult
     public func colorsTheme(_ prop: [Any]) -> AAChartModel {
-        self.colorsTheme = prop
+        colorsTheme = prop
         return self
     }
     
     @discardableResult
     public func series(_ prop: [[String: Any]]) -> AAChartModel {
-        self.series = prop
+        series = prop
         return self
     }
     
     @discardableResult
     public func legendEnabled(_ prop: Bool) -> AAChartModel {
-        self.legendEnabled = prop
+        legendEnabled = prop
         return self
     }
     
     @discardableResult
     public func legendLayout(_ prop: AAchartLegendlLayoutType) -> AAChartModel {
-        self.legendLayout = prop.rawValue
+        legendLayout = prop.rawValue
         return self
     }
     
     @discardableResult
     public func legendAlign(_ prop: AAChartLegendAlignType) -> AAChartModel {
-        self.legendAlign = prop.rawValue
+        legendAlign = prop.rawValue
         return self
     }
     
     @discardableResult
     public func legendVerticalAlign(_ prop: AAChartLegendVerticalAlignType) -> AAChartModel {
-        self.legendAlign = prop.rawValue
+        legendAlign = prop.rawValue
         return self
     }
     
     @discardableResult
     public func backgroundColor(_ prop: String) -> AAChartModel {
-        self.backgroundColor = prop
+        backgroundColor = prop
         return self
     }
     
     @discardableResult
     public func titleColor(_ prop: String) -> AAChartModel {
-        self.titleColor = prop
+        titleColor = prop
         return self
     }
     
     @discardableResult
     public func subTitleColor(_ prop: String) -> AAChartModel {
-        self.subTitleColor = prop
+        subTitleColor = prop
         return self
     }
     
     @discardableResult
     public func axisColor(_ prop: String) -> AAChartModel {
-        self.axisColor = prop
+        axisColor = prop
         return self
     }
     
     @discardableResult
     public func borderRadius(_ prop: Int) -> AAChartModel {
-        self.borderRadius = prop
+        borderRadius = prop
         return self
     }
     
     @discardableResult
     public func markerRadius(_ prop: Int) -> AAChartModel {
-        self.markerRadius = prop
+        markerRadius = prop
         return self
     }
     
     
     public  init() {
-        self.backgroundColor     = "#ffffff"
-        self.titleColor          = "#000000"
-        self.subTitleColor       = "#000000"
-        self.axisColor           = "#000000"
-        self.animationDuration   = 800//以毫秒为单位
-        self.chartType           = AAChartType.Column.rawValue
-        self.inverted            = false
-        self.stacking            = AAChartStackingType.False.rawValue
-        self.xAxisReversed       = false
-        self.yAxisReversed       = false
-        self.zoomType            = AAChartZoomType.None.rawValue//默认禁用手势缩放
-        self.colorsTheme         = ["#1e90ff","#ef476f","#ffd066","#04d69f","#25547c",]
-        self.gradientColorEnable = false
-        self.polar               = false
-        self.dataLabelEnabled    = true
-        self.tooltipEnabled      = true
-        self.tooltipCrosshairs   = true
-        self.xAxisLabelsEnabled  = true
-        self.xAxisVisible        = true  // X 轴默认可见
-        self.yAxisVisible        = true  // Y 轴默认可见
-        self.yAxisLabelsEnabled  = true
-        self.yAxisLineWidth      = 0
-        self.yAxisGridLineWidth  = 0.6
-        self.legendEnabled       = true
-        self.legendLayout        = AAchartLegendlLayoutType.Horizontal.rawValue
-        self.legendAlign         = AAChartLegendAlignType.Center.rawValue
-        self.legendVerticalAlign = AAChartLegendVerticalAlignType.Bottom.rawValue
-        self.borderRadius        = 0//柱状图长条图头部圆角半径(可用于设置头部的形状,仅对条形图,柱状图有效,设置为1000时,柱形图或者条形图头部为楔形)
-        self.markerRadius        = 5//折线连接点的半径长度,设置默认值为0,这样就相当于不显示了
+        backgroundColor     = "#ffffff"
+        titleColor          = "#000000"
+        subTitleColor       = "#000000"
+        axisColor           = "#000000"
+        animationDuration   = 800//以毫秒为单位
+        chartType           = AAChartType.Column.rawValue
+        inverted            = false
+        stacking            = AAChartStackingType.False.rawValue
+        xAxisReversed       = false
+        yAxisReversed       = false
+        zoomType            = AAChartZoomType.None.rawValue//默认禁用手势缩放
+        colorsTheme         = ["#1e90ff","#ef476f","#ffd066","#04d69f","#25547c",]
+        gradientColorEnable = false
+        polar               = false
+        dataLabelEnabled    = true
+        tooltipEnabled      = true
+        tooltipCrosshairs   = true
+        xAxisLabelsEnabled  = true
+        xAxisVisible        = true  // X 轴默认可见
+        yAxisVisible        = true  // Y 轴默认可见
+        yAxisLabelsEnabled  = true
+        yAxisLineWidth      = 0
+        yAxisGridLineWidth  = 0.6
+        legendEnabled       = true
+        legendLayout        = AAchartLegendlLayoutType.Horizontal.rawValue
+        legendAlign         = AAChartLegendAlignType.Center.rawValue
+        legendVerticalAlign = AAChartLegendVerticalAlignType.Bottom.rawValue
+        borderRadius        = 0//柱状图长条图头部圆角半径(可用于设置头部的形状,仅对条形图,柱状图有效,设置为1000时,柱形图或者条形图头部为楔形)
+        markerRadius        = 5//折线连接点的半径长度,设置默认值为0,这样就相当于不显示了
     }
     
 }
--- AAInfographics/AAChartCreator/AASeriesElement.swift
+++ AAInfographics/AAChartCreator/AASeriesElement.swift
@@ -31,137 +31,137 @@
  */
 
  public class AASeriesElement: AASerializable {
-    private var type:String?
-    private var name:String?
-    private var data:[Any]?
-    private var color:Any?
-    private var lineWidth:Float?//折线图、曲线图、直方折线图、折线填充图、曲线填充图、直方折线填充图的线条宽度
-    private var borderWidth:Float?//柱形图、条形图、扇形图等图形的描边宽度
-    private var borderColor:String?//柱形图、条形图、扇形图等图形的描边颜色
-    private var fillColor:Any?//折线填充图、曲线填充图、直方折线填充图等填充图类型的填充颜色
-    private var fillOpacity:Float?//折线填充图、曲线填充图、直方折线填充图等填充图类型的填充颜色透明度 Fill opacity for the area. Note that when you set an explicit fillColor, the fillOpacity is not applied. Instead, you should define the opacity in the fillColor with an rgba color definition. 默认是：0.75. https://api.hcharts.cn/highcharts#plotOptions.area.fillOpacity
-    private var threshold:Float?//The threshold, also called zero level or base level. For line type series this is only used in conjunction with negativeColor. default：0.
-    private var negativeColor:String?// The color for the parts of the graph or points that are below the threshold
-    private var dashStyle:String?
-    private var dataLabels:[String: Any]?
-    private var marker:[String: Any]?
-    private var step:Any?
-    private var colorByPoint:Bool?
-    private var allowPointSelect:Bool?
-    private var zIndex:Int?
-    private var innerSize:String?//内部圆环半径大小占比(内部圆环半径/扇形图半径)
+    private var type: String?
+    private var name: String?
+    private var data: [Any]?
+    private var color: Any?
+    private var lineWidth: Float?//折线图、曲线图、直方折线图、折线填充图、曲线填充图、直方折线填充图的线条宽度
+    private var borderWidth: Float?//柱形图、条形图、扇形图等图形的描边宽度
+    private var borderColor: String?//柱形图、条形图、扇形图等图形的描边颜色
+    private var fillColor: Any?//折线填充图、曲线填充图、直方折线填充图等填充图类型的填充颜色
+    private var fillOpacity: Float?//折线填充图、曲线填充图、直方折线填充图等填充图类型的填充颜色透明度 Fill opacity for the area. Note that when you set an explicit fillColor, the fillOpacity is not applied. Instead, you should define the opacity in the fillColor with an rgba color definition. 默认是：0.75. https://api.hcharts.cn/highcharts#plotOptions.area.fillOpacity
+    private var threshold: Float?//The threshold, also called zero level or base level. For line type series this is only used in conjunction with negativeColor. default：0.
+    private var negativeColor: String?// The color for the parts of the graph or points that are below the threshold
+    private var dashStyle: String?
+    private var dataLabels: [String: Any]?
+    private var marker: [String: Any]?
+    private var step: Any?
+    private var colorByPoint: Bool?
+    private var allowPointSelect: Bool?
+    private var zIndex: Int?
+    private var innerSize: String?//内部圆环半径大小占比(内部圆环半径/扇形图半径)
 
     @discardableResult
-    public func type(_ prop:AAChartType) -> AASeriesElement {
-        self.type = prop.rawValue
+    public func type(_ prop: AAChartType) -> AASeriesElement {
+        type = prop.rawValue
         return self
     }
     
     @discardableResult
-    public func name(_ prop:String) -> AASeriesElement {
-        self.name = prop
+    public func name(_ prop: String) -> AASeriesElement {
+        name = prop
         return self
     }
     
     @discardableResult
-    public func data(_ prop:[Any]) -> AASeriesElement {
-        self.data = prop
+    public func data(_ prop: [Any]) -> AASeriesElement {
+        data = prop
         return self
     }
     
     @discardableResult
-    public func lineWidth(_ prop:Float) -> AASeriesElement {
-        self.lineWidth = prop
+    public func lineWidth(_ prop: Float) -> AASeriesElement {
+        lineWidth = prop
         return self
     }
     
     @discardableResult
-    public func borderWidth(_ prop:Float) -> AASeriesElement {
-        self.borderWidth = prop
+    public func borderWidth(_ prop: Float) -> AASeriesElement {
+        borderWidth = prop
         return self
     }
     
     @discardableResult
-    public func borderColor(_ prop:String) -> AASeriesElement {
-        self.borderColor = prop
+    public func borderColor(_ prop: String) -> AASeriesElement {
+        borderColor = prop
         return self
     }
     
     @discardableResult
-    public func fillColor(_ prop:Any) -> AASeriesElement {
-        self.fillColor = prop
+    public func fillColor(_ prop: Any) -> AASeriesElement {
+        fillColor = prop
         return self
     }
     
     @discardableResult
-    public func color(_ prop:Any) -> AASeriesElement {
-        self.color = prop
+    public func color(_ prop: Any) -> AASeriesElement {
+        color = prop
         return self
     }
     
     @discardableResult
-    public func fillOpacity(_ prop:Float) -> AASeriesElement {
-        self.fillOpacity = prop
+    public func fillOpacity(_ prop: Float) -> AASeriesElement {
+        fillOpacity = prop
         return self
     }
     
     @discardableResult
-    public func threshold(_ prop:Float) -> AASeriesElement {
-        self.threshold = prop
+    public func threshold(_ prop: Float) -> AASeriesElement {
+        threshold = prop
         return self
     }
     
     @discardableResult
-    public func negativeColor(_ prop:String) -> AASeriesElement {
-        self.negativeColor = prop
+    public func negativeColor(_ prop: String) -> AASeriesElement {
+        negativeColor = prop
         return self
     }
     
     @discardableResult
-    public func dashStyle(_ prop:AALineDashSyleType) -> AASeriesElement {
-        self.dashStyle = prop.rawValue
+    public func dashStyle(_ prop: AALineDashSyleType) -> AASeriesElement {
+        dashStyle = prop.rawValue
         return self
     }
     
     @discardableResult
-    public func dataLabels(_ prop:[String: Any]) -> AASeriesElement {
-        self.dataLabels = prop
+    public func dataLabels(_ prop: [String: Any]) -> AASeriesElement {
+        dataLabels = prop
         return self
     }
     
     @discardableResult
-    public func marker(_ prop:[String: Any]) -> AASeriesElement {
-        self.marker = prop
+    public func marker(_ prop: [String: Any]) -> AASeriesElement {
+        marker = prop
         return self
     }
     
     @discardableResult
-    public func step(_ prop:Any) -> AASeriesElement {
-        self.step = prop
+    public func step(_ prop: Any) -> AASeriesElement {
+        step = prop
         return self
     }
     
     @discardableResult
-    public func colorByPoint(_ prop:Bool) -> AASeriesElement {
-        self.colorByPoint = prop
+    public func colorByPoint(_ prop: Bool) -> AASeriesElement {
+        colorByPoint = prop
         return self
     }
     
     @discardableResult
-    public func allowPointSelect(_ prop:Bool) -> AASeriesElement {
-        self.allowPointSelect = prop
+    public func allowPointSelect(_ prop: Bool) -> AASeriesElement {
+        allowPointSelect = prop
         return self
     }
     
     @discardableResult
-    public func zIndex(_ prop:Int) -> AASeriesElement {
-        self.zIndex = prop
+    public func zIndex(_ prop: Int) -> AASeriesElement {
+        zIndex = prop
         return self
     }
     
     @discardableResult
-    public func innerSize(_ prop:String) -> AASeriesElement {
-        self.innerSize = prop
+    public func innerSize(_ prop: String) -> AASeriesElement {
+        innerSize = prop
         return self
     }
 
