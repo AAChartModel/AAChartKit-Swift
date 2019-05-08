c81bef6722d24b84bf1d613b1c626962aa6901de
AnAn <2236368544@qq.com>
2019-05-08 09:49:08 +0800

    Optimize code (simplify code)

--- AAInfographics/AAChartCreator/AAColor.swift
+++ AAInfographics/AAChartCreator/AAColor.swift
@@ -32,94 +32,65 @@
 import UIKit
 
 public class AAColor: NSObject {
-
+    
     public static var black: String! {
-        get {
-            return "black"
-        }
+        return "black"
     } // 0.0 white
     
-   public static var darkGray: String! {
-        get {
-            return "darkGray"
-        }
+    public static var darkGray: String! {
+        return "darkGray"
     } // 0.333 white
     
-   public static var lightGray: String! {
-        get {
-            return "lightGray"
-        }
+    public static var lightGray: String! {
+        return "lightGray"
     } // 0.667 white
     
-   public static var white: String! {
-        get {
-            return "white"
-        }
+    public static var white: String! {
+        return "white"
     } // 1.0 white
     
-   public static var gray: String! {
-        get {
-            return "gray"
-        }
+    public static var gray: String! {
+        return "gray"
     } // 0.5 white
     
-   public static var red: String! {
-        get {
-            return "red"
-        }
+    public static var red: String! {
+        return "red"
     } // 1.0, 0.0, 0.0 RGB
     
-   public static var green: String! {
-        get {
-            return "green"
-        }
+    public static var green: String! {
+        return "green"
     } // 0.0, 1.0, 0.0 RGB
     
-   public static var blue: String! {
-        get {
-            return "blue"
-        }
+    public static var blue: String! {
+        return "blue"
     } // 0.0, 0.0, 1.0 RGB
     
-   public static var cyan: String! {
-        get {
-            return "cyan"
-        }
+    public static var cyan: String! {
+        return "cyan"
     } // 0.0, 1.0, 1.0 RGB
     
-   public static var yellow: String! {
-        get {
-            return "yellow"
-        }
+    public static var yellow: String! {
+        return "yellow"
     } // 1.0, 1.0, 0.0 RGB
     
-   public static var magenta: String! {
-        get {
-            return "magenta"
-        }
+    public static var magenta: String! {
+        return "magenta"
     } // 1.0, 0.0, 1.0 RGB
     
-   public static var orange: String! {
-        get {
-            return "orange"
-        }
+    public static var orange: String! {
+        return "orange"
     } // 1.0, 0.5, 0.0 RGB
     
-   public static var purple: String! {
-        get {
-            return "purple"
-        }
+    public static var purple: String! {
+        return "purple"
     } // 0.5, 0.0, 0.5 RGB
     
-   public static var brown: String! {
-        get {
-            return "brown"
-        }
+    public static var brown: String! {
+        return "brown"
     } // 0.6, 0.4, 0.2 RGB
     
-   public static var clear: String! {
-        get {
-            return "#00000000"
-        }
+    public static var clear: String! {
+        return "#00000000"
     } // 0.0 white, 0.0 alpha
 }
+
--- AAInfographics/AAChartCreator/AAGradientColor.swift
+++ AAInfographics/AAChartCreator/AAGradientColor.swift
@@ -34,147 +34,99 @@ import UIKit
 
 public class AAGradientColor: NSObject  {
     public static var oceanBlue: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "2E3192", endColor: "1BFFFF")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "2E3192", endColor: "1BFFFF")
     }
     
     public static var sanguine: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "D4145A", endColor: "FBB03B")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "D4145A", endColor: "FBB03B")
     }
     
     public static var lusciousLime: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "009245", endColor: "FCEE21")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "009245", endColor: "FCEE21")
     }
     
     public static var purpleLake: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "662D8C", endColor: "ED1E79")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "662D8C", endColor: "ED1E79")
     }
     
     public static var freshPapaya: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "ED1C24", endColor: "FCEE21")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "ED1C24", endColor: "FCEE21")
     }
     
     public static var ultramarine: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "00A8C5", endColor: "FFFF7E")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "00A8C5", endColor: "FFFF7E")
     }
     
     public static var pinkSugar: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "D74177", endColor: "FFE98A")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "D74177", endColor: "FFE98A")
     }
     
     public static var lemonDrizzle: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "FB872B", endColor: "D9E021")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "FB872B", endColor: "D9E021")
     }
     
     public static var victoriaPurple: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "312A6C", endColor: "852D91")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "312A6C", endColor: "852D91")
     }
     
     public static var springGreens: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "009E00", endColor: "FFFF96")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "009E00", endColor: "FFFF96")
     }
     
     public static var mysticMauve: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "B066FE", endColor: "63E2FF")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "B066FE", endColor: "63E2FF")
     }
     
     public static var reflexSilver: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "808080", endColor: "E6E6E6")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "808080", endColor: "E6E6E6")
     }
     
     public static var neonGlow: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "00FFA1", endColor: "00FFFF")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "00FFA1", endColor: "00FFFF")
     }
     
     public static var berrySmoothie: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "8E78FF", endColor: "FC7D7B")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "8E78FF", endColor: "FC7D7B")
     }
     
     public static var newLeaf: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "00537E", endColor: "3AA17E")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "00537E", endColor: "3AA17E")
     }
     
     public static var cottonCandy: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "FCA5F1", endColor: "B5FFFF")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "FCA5F1", endColor: "B5FFFF")
     }
     
     public static var pixieDust: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "D585FF", endColor: "00FFEE")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "D585FF", endColor: "00FFEE")
     }
     
     public static var fizzyPeach: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "F24645", endColor: "EBC08D")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "F24645", endColor: "EBC08D")
     }
     
     public static var sweetDream: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "3A3897", endColor: "A3A1FF")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "3A3897", endColor: "A3A1FF")
     }
     
     public static var firebrick: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "45145A", endColor: "FF5300")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "45145A", endColor: "FF5300")
     }
     
     public static var wroughtIron: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "333333", endColor: "5A5454")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "333333", endColor: "5A5454")
     }
     
     public static var deepSea: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "4F00BC", endColor: "29ABE2")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "4F00BC", endColor: "29ABE2")
     }
     
     public static var coastalBreeze: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "00B7FF", endColor: "FFFFC7")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "00B7FF", endColor: "FFFFC7")
     }
     
     public static var eveningDelight: [String : Any]! {
-        get {
-            return GradientColorDicMaker.configureGradientColorDictionary(startColor: "93278F", endColor: "00A99D")
-        }
+        return GradientColorDicMaker.configureGradientColorDictionary(startColor: "93278F", endColor: "00A99D")
     }
 }
 
@@ -194,3 +146,5 @@ public class GradientColorDicMaker {
             ] as [String : Any]
     }
 }
+
+
