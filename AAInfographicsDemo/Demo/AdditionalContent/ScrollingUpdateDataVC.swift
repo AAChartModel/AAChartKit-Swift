2f7248ca5a56fe8b82f68f5b0c3a54209b37801e
AnAn <2236368544@qq.com>
2019-11-09 23:37:52 +0800

    Add ScrollingUpdateDataVC

---
+++ AAInfographicsDemo/Demo/AdditionalContent/ScrollingUpdateDataVC.swift
@@ -0,0 +1,30 @@
+//
+//  ScrollingUpdateDataVC.swift
+//  AAInfographicsDemo
+//
+//  Created by AnAn on 2019/11/9.
+//  Copyright © 2019 An An. All rights reserved.
+//
+
+import UIKit
+
+class ScrollingUpdateDataVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
