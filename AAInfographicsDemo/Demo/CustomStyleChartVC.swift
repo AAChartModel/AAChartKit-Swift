1d8ef6cc7027904de26e6bfb275c53946fadab77
AnAn <2236368544@qq.com>
2020-01-08 15:23:53 +0800

    Remove redundant `()`

--- AAInfographicsDemo/Demo/CustomStyleChartVC.swift
+++ AAInfographicsDemo/Demo/CustomStyleChartVC.swift
@@ -135,14 +135,13 @@ class CustomStyleChartVC: UIViewController {
             .categories(colorsNameArr)
             .colorsTheme(colorsArr as [Any])
             .stacking(.percent)
-            .series(([
+            .series([
                 AASeriesElement()
                     .name("Tokyo")
                     .data([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6, 5.5])
                     .colorByPoint(true)
                 ,
                 ])
-        )
     }
     
     private func configureColorfulGradientColorBarChart() -> AAChartModel {
