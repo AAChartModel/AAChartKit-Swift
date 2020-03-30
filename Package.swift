42d3a81dc68704fa79e7797b2ca15a769c2ecf6a
Matovsky <38492784+Matovsky@users.noreply.github.com>
2020-03-30 22:49:05 +0800

    Support Swift Package Manager in Xcode 11

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+     name: "AAInfographics",
+     platforms: [
+         .iOS(.v9)
+     ],
+     products: [
+         .library(name: "AAInfographics", targets: ["AAInfographics"])
+     ],
+     targets: [
+        .target(
+               name: "AAInfographics"
+        )
+     ]
+ )
