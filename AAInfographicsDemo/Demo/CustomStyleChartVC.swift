36cc78579ef226dd6a48b1ba00874c4397265169
AnAn <2236368544@qq.com>
2021-02-19 15:30:54 +0800

    Update CustomStyleChartVC.swift

--- AAInfographicsDemo/Demo/CustomStyleChartVC.swift
+++ AAInfographicsDemo/Demo/CustomStyleChartVC.swift
@@ -1243,10 +1243,7 @@ class CustomStyleChartVC: AABaseChartVC {
                     .lineWidth(6)
                     .step((true))
                     .dataLabels(AADataLabels()
-                        .style(AAStyle()
-                            .color("#1e90ff")
-                            .fontSize(11)
-                    ))
+                        .style(AAStyle(color: "#1e90ff", fontSize: 11)))
                     .data([2.10, 2.54, 2.78, 3.62, 4.41, 4.09, 3.83, 4.47, 4.20, 3.94, 3.80, 3.58, 3.19, 4.30, 3.69, 3.52, 3.02, 3.30]),
                 AASeriesElement()
                     .name("2018")
@@ -1254,10 +1251,7 @@ class CustomStyleChartVC: AABaseChartVC {
                     .lineWidth(6)
                     .step((true))
                     .dataLabels(AADataLabels()
-                        .style(AAStyle()
-                            .color("#ef476f")
-                            .fontSize(11)
-                    ))
+                        .style(AAStyle(color: "#ef476f", fontSize: 11)))
                     .data([1.56, 1.91, 2.45, 3.87, 3.24, 4.90, 4.61, 4.10, 4.17, 3.85, 4.17, 3.46, 3.46, 3.55, 3.50, 4.13, 2.58, 2.28]),
                 AASeriesElement()
                     .name("2019")
@@ -1265,10 +1259,7 @@ class CustomStyleChartVC: AABaseChartVC {
                     .lineWidth(6)
                     .step((true))
                     .dataLabels(AADataLabels()
-                        .style(AAStyle()
-                            .color("#ffd066")
-                            .fontSize(11)
-                    ))
+                        .style(AAStyle(color: "#ffd066", fontSize: 11)))
                     .data([1.16, 1.67, 2.64, 2.86, 3.00, 3.21, 4.14, 4.07, 3.68, 3.11, 3.41, 3.25, 3.32, 3.07, 3.92, 3.05, 2.18, 3.24]),
                 AASeriesElement()
                     .name("2020")
@@ -1276,10 +1267,7 @@ class CustomStyleChartVC: AABaseChartVC {
                     .lineWidth(6)
                     .step((true))
                     .dataLabels(AADataLabels()
-                        .style(AAStyle()
-                            .color("#04d69f")
-                            .fontSize(11)
-                    ))
+                        .style(AAStyle(color: "#04d69f", fontSize: 11)))
                     .data([5.59, 3.09, 4.09, 6.14, 5.33, 6.05, 5.71, 6.22, 6.56, 4.75, 5.27, 6.02, 5.22, 5.77, 6.19, 5.68, 4.33, 5.48]),
             ])
     }
