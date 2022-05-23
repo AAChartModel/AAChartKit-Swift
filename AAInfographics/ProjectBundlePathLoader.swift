81031ca861041c57122de508a48943ab6ec66046
An An <2236368544@qq.com>
2022-05-23 17:58:59 +0800

    Update ProjectBundlePathLoader.swift

--- AAInfographics/ProjectBundlePathLoader.swift
+++ AAInfographics/ProjectBundlePathLoader.swift
@@ -1,7 +1,18 @@
 import Foundation
 
 class BundlePathLoader {
-	func path(forResource name: String, ofType fileType: String, inDirectory subpath: String? = nil, forLocalization localizationName: String? = nil) -> String? {
-		return Bundle(for: type(of: self)).path(forResource: name, ofType: fileType, inDirectory: subpath, forLocalization: localizationName)
-	}
+    func path(
+        forResource name: String,
+        ofType fileType: String,
+        inDirectory subpath: String? = nil,
+        forLocalization localizationName: String? = nil
+    ) -> String? {
+        return Bundle(for: type(of: self))
+            .path(
+                forResource: name,
+                ofType: fileType,
+                inDirectory: subpath,
+                forLocalization: localizationName
+            )
+    }
 }
