9647525c6d8eb3aea5b91dac009e20ba664ce385
An An <2236368544@qq.com>
2022-05-24 11:58:21 +0800

    Update ProjectBundlePathLoader.swift

--- AAInfographics/ProjectBundlePathLoader.swift
+++ AAInfographics/ProjectBundlePathLoader.swift
@@ -1,3 +1,35 @@
+//
+//  PackageBundlePathLoader.swift
+//  AAInfographicsDemo
+//
+//  Created by Aleksandar Vacić on 2020/12/15.
+//  Copyright © 2022 Aleksandar Vacić. All rights reserved.
+//*************** ...... SOURCE CODE ...... ***************
+//***...................................................***
+//*** https://github.com/AAChartModel/AAChartKit        ***
+//*** https://github.com/AAChartModel/AAChartKit-Swift  ***
+//***...................................................***
+//*************** ...... SOURCE CODE ...... ***************
+
+/*
+ 
+ * -------------------------------------------------------------------------------
+ *
+ *  🌕 🌖 🌗 🌘  ❀❀❀   WARM TIPS!!!   ❀❀❀ 🌑 🌒 🌓 🌔
+ *
+ * Please contact me on GitHub,if there are any problems encountered in use.
+ * GitHub Issues : https://github.com/AAChartModel/AAChartKit-Swift/issues
+ * -------------------------------------------------------------------------------
+ * And if you want to contribute for this project, please contact me as well
+ * GitHub        : https://github.com/AAChartModel
+ * StackOverflow : https://stackoverflow.com/users/12302132/codeforu
+ * JianShu       : https://www.jianshu.com/u/f1e6753d4254
+ * SegmentFault  : https://segmentfault.com/u/huanghunbieguan
+ *
+ * -------------------------------------------------------------------------------
+ 
+ */
+
 import Foundation
 
 class BundlePathLoader {
