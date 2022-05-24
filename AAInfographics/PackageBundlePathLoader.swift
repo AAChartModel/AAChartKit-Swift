ca987decb82fbb19fafb3e6a6db9efb0f23703a8
An An <2236368544@qq.com>
2022-05-24 11:15:00 +0800

    Add PackageBundlePathLoader to resolve SPM bundle issue

---
+++ AAInfographics/PackageBundlePathLoader.swift
@@ -0,0 +1,15 @@
+//
+//  PackageBundlePathLoader.swift
+//  AAInfographicsDemo
+//
+//  Created by An An on 2022/5/24.
+//  Copyright © 2022 An An. All rights reserved.
+//
+
+import Foundation
+
+class BundlePathLoader {
+    func path(forResource name: String, ofType fileType: String, inDirectory subpath: String? = nil, forLocalization localizationName: String? = nil) -> String? {
+        return Bundle.module.path(forResource: name, ofType: fileType, inDirectory: subpath, forLocalization: localizationName)
+    }
+}
