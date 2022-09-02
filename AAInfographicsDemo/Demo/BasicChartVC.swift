37e68e37f75f4fc5c3ed8b86ebbb6ea435c0e9e6
An An <2236368544@qq.com>
2022-09-02 22:17:23 +0800

    Update BasicChartVC.swift

--- AAInfographicsDemo/Demo/BasicChartVC.swift
+++ AAInfographicsDemo/Demo/BasicChartVC.swift
@@ -377,11 +377,11 @@ class BasicChartVC: UIViewController {
         aaChartView!.aa_refreshChartWholeContentWithChartModel(aaChartModel!)
     }
     
-   private func kRGBColorFromHex(rgbValue: Int) -> (UIColor) {
-        return UIColor(red: ((CGFloat)((rgbValue & 0xFF0000) >> 16)) / 255.0,
-                       green: ((CGFloat)((rgbValue & 0xFF00) >> 8)) / 255.0,
-                       blue: ((CGFloat)(rgbValue & 0xFF)) / 255.0,
-                       alpha: 1.0)
+   private func kRGBColorFromHex(rgbValue: Int) -> UIColor {
+       UIColor(red: ((CGFloat)((rgbValue & 0xFF0000) >> 16)) / 255.0,
+               green: ((CGFloat)((rgbValue & 0xFF00) >> 8)) / 255.0,
+               blue: ((CGFloat)(rgbValue & 0xFF)) / 255.0,
+               alpha: 1.0)
     }
 }
 
