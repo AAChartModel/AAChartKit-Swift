7f4b2d1517177bad285d6d2bf5a5677cf61d9081
An An <2236368544@qq.com>
2022-09-28 17:54:22 +0800

    Add JSFunctionForAAAxisVC

---
+++ AAInfographicsDemo/Demo/AAOptionsWithJS/JSFunctionForAAAxisVC.swift
@@ -0,0 +1,30 @@
+//
+//  JSFunctionForAAAxisVC.swift
+//  AAInfographicsDemo
+//
+//  Created by AnAn on 2022/9/28.
+//  Copyright © 2022 An An. All rights reserved.
+//
+
+import UIKit
+
+class JSFunctionForAAAxisVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
