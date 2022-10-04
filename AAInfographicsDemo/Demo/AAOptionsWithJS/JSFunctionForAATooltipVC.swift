9ec265c5f5efd79222819f814dcab9d4c16cd165
An An <2236368544@qq.com>
2022-10-04 16:34:58 +0800

    Update JSFunctionForAATooltipVC.swift

--- AAInfographicsDemo/Demo/AAOptionsWithJS/JSFunctionForAATooltipVC.swift
+++ AAInfographicsDemo/Demo/AAOptionsWithJS/JSFunctionForAATooltipVC.swift
@@ -48,17 +48,15 @@ class JSFunctionForAATooltipVC: AABaseChartVC {
         case  3: return customLineChartTooltipStyleWhenValueBeZeroDoNotShow()
         case  4: return customBoxplotTooltipContent()
         case  5: return customStackedAndGroupedColumnChartTooltip()
-        case  6: return customDoubleXAxesChart()
-        case  7: return customArearangeChartTooltip()
-        case  8: return customLineChartOriginalPointPositionByConfiguringXAxisFormatterAndTooltipFormatter()
-        case  9: return customTooltipWhichDataSourceComeFromOutSideRatherThanSeries()
-        case 10: return customTooltipPositionerFunction()
-        case 11: return fixedTooltipPositionByCustomPositionerFunction()
-        case 12: return disableColumnChartUnselectEventEffectBySeriesPointEventClickFunction()
-        case 14: return customAreasplineChartTooltipStyleByDivWithCSS()
-        case 15: return makePieChartShow0Data()
-        case 16: return customColumnChartBorderStyleAndStatesHoverColor()
-        case 17: return customPlotAreaOutsideComlicatedTooltipStyle()
+        case  6: return customArearangeChartTooltip()
+        case  7: return customLineChartOriginalPointPositionByConfiguringXAxisFormatterAndTooltipFormatter()
+        case  8: return customTooltipWhichDataSourceComeFromOutSideRatherThanSeries()
+        case  9: return customTooltipPositionerFunction()
+        case 10: return fixedTooltipPositionByCustomPositionerFunction()
+        case 11: return customAreasplineChartTooltipStyleByDivWithCSS()
+        case 12: return makePieChartShow0Data()
+        case 13: return customColumnChartBorderStyleAndStatesHoverColor()
+        case 14: return customPlotAreaOutsideComlicatedTooltipStyle()
 
 
         default:
@@ -388,112 +386,7 @@ function () {
         
         return aaOptions
     }
-    
-    //https://github.com/AAChartModel/AAChartKit/issues/780
-    private func customDoubleXAxesChart() -> AAOptions {
-        let gradientColorDic1 = AAGradientColor.linearGradient(
-            direction: .toTop,
-            startColor: "#7052f4",
-            endColor: "#00b0ff"//颜色字符串设置支持十六进制类型和 rgba 类型
-        )
-        
-        let gradientColorDic2 = AAGradientColor.linearGradient(
-            direction: .toTop,
-            startColor: "#EF71FF",
-            endColor: "#4740C8"//颜色字符串设置支持十六进制类型和 rgba 类型
-        )
-        
-        let aaChart = AAChart()
-            .type(.bar)
-        
-        let aaTitle = AATitle()
-            .text("2015 年德国人口金字塔")
-            .style(AAStyle()
-                .color(AAColor.black)
-                .fontSize(12.0))
-        
-        let aaCategories = [
-            "0-4", "5-9", "10-14", "15-19",
-            "20-24", "25-29", "30-34", "35-39", "40-44",
-            "45-49", "50-54", "55-59", "60-64", "65-69",
-            "70-74", "75-79", "80-84", "85-89", "90-94",
-            "95-99", "100 + "
-        ]
-        
-        let aaXAxis1 = AAXAxis()
-            .reversed(true)
-            .categories(aaCategories)
-            .labels(AALabels()
-                .step(1))
-        
-        let aaXAxis2 = AAXAxis()
-            .reversed(true)
-            .opposite(true)
-            .categories(aaCategories)
-            .linkedTo(0)
-            .labels(AALabels()
-                .step(1))
-        
-        let aaYAxis = AAYAxis()
-            .gridLineWidth(0)// Y 轴网格线宽度
-            .title(AATitle()
-                .text(""))//Y 轴标题
-            .labels(AALabels()
-                .formatter("""
-function () {
-    return (Math.abs(this.value) / 1000000) + 'M';
-}
-"""))
-            .min( -4000000)
-            .max( 4000000)
-        
-        let aaPlotOptions = AAPlotOptions()
-            .series(AASeries()
-                .animation(AAAnimation()
-                    .duration(800)
-                    .easing(.bounce))
-                .stacking(.normal))
-        
-        let aaTooltip = AATooltip()
-            .enabled(true)
-            .shared(false)
-            .formatter("""
-function () {
-    return '<b>' + this.series.name + ', age ' + this.point.category + '</b><br/>' +
-        '人口: ' + Highcharts.numberFormat(Math.abs(this.point.y), 0);
-}
-""")
-        
-        let aaSeriesElement1 = AASeriesElement()
-            .name("Men")
-            .color(gradientColorDic1)
-            .data([
-                -1746181, -1884428, -2089758, -2222362, -2537431, -2507081, -2443179,
-                -2664537, -3556505, -3680231, -3143062, -2721122, -2229181, -2227768,
-                -2176300, -1329968, -836804, -354784, -90569, -28367, -3878
-            ])
-        
-        let aaSeriesElement2 = AASeriesElement()
-            .name("Women")
-            .color(gradientColorDic2)
-            .data([
-                1656154, 1787564, 1981671, 2108575, 2403438, 2366003, 2301402, 2519874,
-                3360596, 3493473, 3050775, 2759560, 2304444, 2426504, 2568938, 1785638,
-                1447162, 1005011, 330870, 130632, 21208
-            ])
-        
-        let aaOptions = AAOptions()
-            .chart(aaChart)
-            .title(aaTitle)
-            .xAxisArray([aaXAxis1,aaXAxis2])
-            .yAxis(aaYAxis)
-            .plotOptions(aaPlotOptions)
-            .tooltip(aaTooltip)
-            .series([aaSeriesElement1,aaSeriesElement2])
-        
-        return aaOptions
-    }
-    
+
     //https://github.com/AAChartModel/AAChartKit/issues/781
     private func customArearangeChartTooltip() -> AAOptions {
         let aaChartModel = AAChartModel()
@@ -792,44 +685,6 @@ function () {
             }
             """)
         
-        return aaOptions
-    }
-    
-    //https://github.com/AAChartModel/AAChartKit/issues/967
-    private func disableColumnChartUnselectEventEffectBySeriesPointEventClickFunction() -> AAOptions  {
-        let aaChartModel = AAChartModel()
-            .chartType(.bar)
-            .title("Custom Bar Chart select color")
-            .yAxisTitle("")
-            .yAxisReversed(true)
-            .xAxisReversed(true)
-            .series([
-                AASeriesElement()
-                    .name("ElementOne")
-                    .data([211,183,157,133,111,91,73,57,43,31,21,13,7,3])
-                    .allowPointSelect(true)
-                    .states(AAStates()
-                        .hover(AAHover()
-                            .color(AAColor.yellow))
-                        .select(AASelect()
-                            .color(AAColor.red)))
-            ])
-        
-        let aaOptions = aaChartModel.aa_toAAOptions()
-        
-        aaOptions.plotOptions?.series?
-            .point(AAPoint()
-                .events(AAPointEvents()
-                    .click("""
-                    function () {
-                        if (this.selected == true) {
-                            this.selected = false;
-                        }
-                        return;
-                    }
-                    """))
-        )
-
         return aaOptions
     }
     
