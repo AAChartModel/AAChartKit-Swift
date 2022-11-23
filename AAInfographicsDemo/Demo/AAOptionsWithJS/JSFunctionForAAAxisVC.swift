6497a3fe7bd8fe193298e3f9618c9d69fdc16c28
An An <2236368544@qq.com>
2022-11-23 16:17:47 +0800

    Update JSFunctionForAAAxisVC.swift

--- AAInfographicsDemo/Demo/AAOptionsWithJS/JSFunctionForAAAxisVC.swift
+++ AAInfographicsDemo/Demo/AAOptionsWithJS/JSFunctionForAAAxisVC.swift
@@ -207,7 +207,7 @@ function () {
                 .series(AASeries()
                         .marker(AAMarker()
                                 .radius(7)//曲线连接点半径，默认是4
-                                .symbol(AAChartSymbolType.circle.rawValue)//曲线点类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
+                                .symbol(.circle)//曲线点类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
                                 .fillColor(AAColor.white)//点的填充色(用来设置折线连接点的填充色)
                                 .lineWidth(3)//外沿线的宽度(用来设置折线连接点的轮廓描边的宽度)
                                 .lineColor("")//外沿线的颜色(用来设置折线连接点的轮廓描边颜色，当值为空字符串时，默认取数据点或数据列的颜色)
@@ -335,7 +335,7 @@ function () {
             .series(AASeries()
                 .marker(AAMarker()
                     .radius(7)//曲线连接点半径，默认是4
-                    .symbol(AAChartSymbolType.circle.rawValue)//曲线点类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
+                    .symbol(.circle)//曲线点类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
                     .fillColor(AAColor.white)//点的填充色(用来设置折线连接点的填充色)
                     .lineWidth(3)//外沿线的宽度(用来设置折线连接点的轮廓描边的宽度)
                     .lineColor("")//外沿线的颜色(用来设置折线连接点的轮廓描边颜色，当值为空字符串时，默认取数据点或数据列的颜色)
@@ -448,7 +448,7 @@ function () {
             .series(AASeries()
                 .marker(AAMarker()
                     .radius(7)//曲线连接点半径，默认是4
-                    .symbol(AAChartSymbolType.circle.rawValue)//曲线点类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
+                    .symbol(.circle)//曲线点类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
                     .fillColor(AAColor.white)//点的填充色(用来设置折线连接点的填充色)
                     .lineWidth(3)//外沿线的宽度(用来设置折线连接点的轮廓描边的宽度)
                     .lineColor("")//外沿线的颜色(用来设置折线连接点的轮廓描边颜色，当值为空字符串时，默认取数据点或数据列的颜色)
