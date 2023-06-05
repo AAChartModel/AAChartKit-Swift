f1979b275fe5dd7f16350cca756aec3c5cfd30a1
An An <2236368544@qq.com>
2023-06-05 16:33:40 +0800

    Update MainVC.swift

--- AAInfographicsDemo/Demo/MainVC.swift
+++ AAInfographicsDemo/Demo/MainVC.swift
@@ -184,7 +184,8 @@ class MainVC: UIViewController {
                 "configureSpecialStyleColumnForNegativeDataMixedPositiveData---为正负数混合的柱形图自定义特殊样式效果",
                 "configureMultiLevelStopsArrGradientColorAreasplineMixedLineChart---多层次半透明渐变效果的曲线填充图混合折线图📈",
                 "connectNullsForSingleAASeriesElement---为单个 AASeriesElement 单独设置是否断点重连",
-                "lineChartsWithLargeDifferencesInTheNumberOfDataInDifferentSeriesElement---测试有多组数据时, 数据量较大时, 不同组数据量差距较大时的折线图📈"
+                "lineChartsWithLargeDifferencesInTheNumberOfDataInDifferentSeriesElement---测试有多组数据时, 数据量较大时, 不同组数据量差距较大时的折线图📈",
+                "customAreasplineChartWithColorfulGradientColorZones---彩色渐变色区域填充图",
 
             ],
             /*Mixed Chart*/
