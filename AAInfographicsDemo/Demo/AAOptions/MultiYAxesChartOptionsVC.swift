51b03fc6b96fa8a461c140ab45b3af27f35ec20f
An An <2236368544@qq.com>
2023-07-24 18:25:57 +0800

    Update MultiYAxesChartOptionsVC.swift

--- AAInfographicsDemo/Demo/AAOptions/MultiYAxesChartOptionsVC.swift
+++ AAInfographicsDemo/Demo/AAOptions/MultiYAxesChartOptionsVC.swift
@@ -97,70 +97,73 @@ class MultiYAxesChartOptionsVC: AABaseChartVC {
     //        data: [144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2]
     //    }]
     //});
+    
+    //https://api.highcharts.com/highcharts/xAxis.reversedStacks
     private func doubleXAxesAndDoubleYAxesChart() -> AAOptions {
         AAOptions()
-                .chart(AAChart()
-                        .type(.column))
-                .title(AATitle()
-                        .text("Different xAxis.reversedStacks behaviour."))
-                .xAxisArray([
-                    AAXAxis()
-                            .height("50%")
-                            .opposite(true)
-                            .reversed(true)
-                            .reversedStacks(false)
-                            .categories(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
-                                         "Nov", "Dec"]),
-                    AAXAxis()
-                            .top("50%")
-                            .height("50%")
-                            .reversed(true)
-                            .reversedStacks(true)
-                            .categories(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
-                                         "Nov", "Dec"])
-                ])
-                .yAxisArray([
-                    AAYAxis()
-                            .height("50%")
-                            .offset(0)
-                            .title(AATitle()
-                                    .text("reversedStacks: <br>false")),
-                    AAYAxis()
-                            .height("50%")
-                            .top("50%")
-                            .offset(0)
-                            .title(AATitle()
-                                    .text("reversedStacks: <br>true"))
-                ])
-                .plotOptions(AAPlotOptions()
-                        .series(AASeries()
-                                .stacking(.normal)))
-                .series([
-                    AASeriesElement()
-                            .stack("first")
-                            .color(AAColor.red)
-                            .data([29.9, 71.5, 106.4, 129.2, 144.0, 176.0,
-                                   135.6, 148.5, 216.4, 194.1, 95.6, 54.4]),
-                    AASeriesElement()
-                            .stack("second")
-                            .color(AAColor.green)
-                            .data([144.0, 176.0, 135.6, 148.5, 216.4, 194.1,
-                                   95.6, 54.4, 29.9, 71.5, 106.4, 129.2]),
-                    AASeriesElement()
-                            .xAxis(1)
-                            .yAxis(1)
-                            .stack("third")
-                            .color(AAColor.red)
-                            .data([29.9, 71.5, 106.4, 129.2, 144.0, 176.0,
-                                   135.6, 148.5, 216.4, 194.1, 95.6, 54.4]),
-                    AASeriesElement()
-                            .xAxis(1)
-                            .yAxis(1)
-                            .stack("fourth")
-                            .color(AAColor.green)
-                            .data([144.0, 176.0, 135.6, 148.5, 216.4, 194.1,
-                                   95.6, 54.4, 29.9, 71.5, 106.4, 129.2])
-                ])
+            .chart(AAChart()
+                .type(.column))
+            .title(AATitle()
+                .text("Different xAxis.reversedStacks behaviour."))
+            .xAxisArray([
+                AAXAxis()
+                    .height("50%")
+                    .opposite(true)
+                    .reversed(true)
+                    .reversedStacks(false)
+                    .categories(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
+                                 "Nov", "Dec"]),
+                AAXAxis()
+                    .top("50%")
+                    .height("50%")
+                    .reversed(true)
+                    .reversedStacks(true)
+                    .categories(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
+                                 "Nov", "Dec"])
+            ])
+            .yAxisArray([
+                AAYAxis()
+                    .height("50%")
+                    .offset(0)
+                    .reversed(true)
+                    .title(AATitle()
+                        .text("reversedStacks: <br>false")),
+                AAYAxis()
+                    .height("50%")
+                    .top("50%")
+                    .offset(0)
+                    .title(AATitle()
+                        .text("reversedStacks: <br>true"))
+            ])
+            .plotOptions(AAPlotOptions()
+                .series(AASeries()
+                    .stacking(.normal)))
+            .series([
+                AASeriesElement()
+                    .stack("first")
+                    .color(AAColor.red)
+                    .data([29.9, 71.5, 106.4, 129.2, 144.0, 176.0,
+                           135.6, 148.5, 216.4, 194.1, 95.6, 54.4]),
+                AASeriesElement()
+                    .stack("second")
+                    .color(AAColor.green)
+                    .data([144.0, 176.0, 135.6, 148.5, 216.4, 194.1,
+                           95.6, 54.4, 29.9, 71.5, 106.4, 129.2]),
+                AASeriesElement()
+                    .xAxis(1)
+                    .yAxis(1)
+                    .stack("third")
+                    .color(AAColor.red)
+                    .data([29.9, 71.5, 106.4, 129.2, 144.0, 176.0,
+                           135.6, 148.5, 216.4, 194.1, 95.6, 54.4]),
+                AASeriesElement()
+                    .xAxis(1)
+                    .yAxis(1)
+                    .stack("fourth")
+                    .color(AAColor.green)
+                    .data([144.0, 176.0, 135.6, 148.5, 216.4, 194.1,
+                           95.6, 54.4, 29.9, 71.5, 106.4, 129.2])
+            ])
     }
 
 }
