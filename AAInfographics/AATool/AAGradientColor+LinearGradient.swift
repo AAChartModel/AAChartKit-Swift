e9d4c4fed27341f06f8f44f87f2e541a1d603c8e
An An <2236368544@qq.com>
2024-02-05 11:33:42 +0800

    Update AAGradientColor+LinearGradient.swift

--- AAInfographics/AATool/AAGradientColor+LinearGradient.swift
+++ AAInfographics/AATool/AAGradientColor+LinearGradient.swift
@@ -90,22 +90,14 @@ extension AAGradientColor {
         direction: AALinearGradientDirection
     ) -> [String : Int] {
         switch direction {
-        case .toTop:
-            return ["x1":0, "y1":1, "x2":0, "y2":0]
-        case .toBottom:
-            return ["x1":0, "y1":0, "x2":0, "y2":1]
-        case .toLeft:
-            return ["x1":1, "y1":0, "x2":0, "y2":0]
-        case .toRight:
-            return ["x1":0, "y1":0, "x2":1, "y2":0]
-        case .toTopLeft:
-            return ["x1":1, "y1":1, "x2":0, "y2":0]
-        case .toTopRight:
-            return ["x1":0, "y1":1, "x2":1, "y2":0]
-        case .toBottomLeft:
-            return ["x1":1, "y1":0, "x2":0, "y2":1]
-        case .toBottomRight:
-            return ["x1":0, "y1":0, "x2":1, "y2":1]
+        case .toTop:         return ["x1":0, "y1":1, "x2":0, "y2":0]
+        case .toBottom:      return ["x1":0, "y1":0, "x2":0, "y2":1]
+        case .toLeft:        return ["x1":1, "y1":0, "x2":0, "y2":0]
+        case .toRight:       return ["x1":0, "y1":0, "x2":1, "y2":0]
+        case .toTopLeft:     return ["x1":1, "y1":1, "x2":0, "y2":0]
+        case .toTopRight:    return ["x1":0, "y1":1, "x2":1, "y2":0]
+        case .toBottomLeft:  return ["x1":1, "y1":0, "x2":0, "y2":1]
+        case .toBottomRight: return ["x1":0, "y1":0, "x2":1, "y2":1]
         }
     }
 }
