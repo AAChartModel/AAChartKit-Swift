ff054cd1f3da1d5ecd26a18e96085c50fa0864f6
An An <2236368544@qq.com>
2024-03-01 14:56:35 +0800

    Update CustomStyleForBarChartVC.swift

--- AAInfographicsDemo/Demo/AAChartModel/ViewController/CustomStyleForBarChartVC.swift
+++ AAInfographicsDemo/Demo/AAChartModel/ViewController/CustomStyleForBarChartVC.swift
@@ -17,23 +17,23 @@ class CustomStyleForBarChartVC: AABaseChartVC {
 
     override func chartConfigurationWithSelectedChartTypeString(_ selectedChartTypeStr: String) -> Any? {
         switch selectedChartTypeStr {
-        case "colorfulBarChart":return CustomStyleForBarChartComposer.colorfulBarChart()
-        case "colorfulGradientColorBarChart":return CustomStyleForBarChartComposer.colorfulGradientColorBarChart()
-        case "discontinuousDataBarChart":return CustomStyleForBarChartComposer.discontinuousDataBarChart()
-        case "randomColorfulBarChart":return CustomStyleForBarChartComposer.randomColorfulBarChart()
+        case "colorfulBarChart": return CustomStyleForBarChartComposer.colorfulBarChart()
+        case "colorfulGradientColorBarChart": return CustomStyleForBarChartComposer.colorfulGradientColorBarChart()
+        case "discontinuousDataBarChart": return CustomStyleForBarChartComposer.discontinuousDataBarChart()
+        case "randomColorfulBarChart": return CustomStyleForBarChartComposer.randomColorfulBarChart()
 
-        case "noneStackingPolarBarChart":return CustomStyleForBarChartComposer.noneStackingPolarBarChart()
-        case "normalStackingPolarBarChart":return CustomStyleForBarChartComposer.normalStackingPolarBarChart()
-        case "percentStackingPolarBarChart":return CustomStyleForBarChartComposer.percentStackingPolarBarChart()
+        case "noneStackingPolarBarChart": return CustomStyleForBarChartComposer.noneStackingPolarBarChart()
+        case "normalStackingPolarBarChart": return CustomStyleForBarChartComposer.normalStackingPolarBarChart()
+        case "percentStackingPolarBarChart": return CustomStyleForBarChartComposer.percentStackingPolarBarChart()
 
-        case "specialStyleForTheSingleDataElementOfBarChart":return CustomStyleForBarChartComposer.specialStyleForTheSingleDataElementOfBarChart()
-        case "noMoreGroupingAndOverlapEachOtherBarChart":return CustomStyleForBarChartComposer.noMoreGroupingAndOverlapEachOtherBarChart()
-        case "noMoreGroupingAndNestedBarChart":return CustomStyleForBarChartComposer.noMoreGroupingAndNestedBarChart()
-        case "topRoundedCornersStackingBarChart":return CustomStyleForBarChartComposer.topRoundedCornersStackingBarChart()
-        case "freeStyleRoundedCornersStackingBarChart":return CustomStyleForBarChartComposer.freeStyleRoundedCornersStackingBarChart()
+        case "specialStyleForTheSingleDataElementOfBarChart": return CustomStyleForBarChartComposer.specialStyleForTheSingleDataElementOfBarChart()
+        case "noMoreGroupingAndOverlapEachOtherBarChart": return CustomStyleForBarChartComposer.noMoreGroupingAndOverlapEachOtherBarChart()
+        case "noMoreGroupingAndNestedBarChart": return CustomStyleForBarChartComposer.noMoreGroupingAndNestedBarChart()
+        case "topRoundedCornersStackingBarChart": return CustomStyleForBarChartComposer.topRoundedCornersStackingBarChart()
+        case "freeStyleRoundedCornersStackingBarChart": return CustomStyleForBarChartComposer.freeStyleRoundedCornersStackingBarChart()
 
-        case "customBorderStyleAndStatesHoverColorBarChart":return CustomStyleForBarChartComposer.customBorderStyleAndStatesHoverColorBarChart()
-        case "negativeDataMixedPositiveDataBarChart":return CustomStyleForBarChartComposer.negativeDataMixedPositiveDataBarChart()
+        case "customBorderStyleAndStatesHoverColorBarChart": return CustomStyleForBarChartComposer.customBorderStyleAndStatesHoverColorBarChart()
+        case "negativeDataMixedPositiveDataBarChart": return CustomStyleForBarChartComposer.negativeDataMixedPositiveDataBarChart()
 
         default: return nil
         }
