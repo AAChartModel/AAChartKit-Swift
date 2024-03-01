c1f917b9db7efda1cc23227075bfb4b47cffb85d
An An <2236368544@qq.com>
2024-03-01 14:54:51 +0800

    Create CustomStyleForScatterChartVC.swift

---
+++ AAInfographicsDemo/Demo/AAChartModel/ViewController/CustomStyleForScatterChartVC.swift
@@ -0,0 +1,30 @@
+//
+//  CustomStyleForScatterChartVC.swift
+//  AAInfographicsDemo
+//
+//  Created by AnAn on 2024/3/1.
+//  Copyright © 2024 An An. All rights reserved.
+//
+
+import AAInfographics
+
+@available(iOS 10.0, macCatalyst 13.1, *)
+class CustomStyleForScatterChartVC: AABaseChartVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    override func chartConfigurationWithSelectedChartTypeString(_ selectedChartTypeStr: String) -> Any? {
+        switch selectedChartTypeStr {
+        case "colorfulBarChart": return CustomStyleForScatterChartComposer.customScatterChartMarkerSymbolContent()
+        case "colorfulGradientColorBarChart": return CustomStyleForScatterChartComposer.drawLineMixedScatterChartWithPointsCoordinates2()
+            
+        default: return nil
+        }
+    }
+
+}
