1e902b0d945c9e097ba34e1f5014ebebbb718dcf
An An <2236368544@qq.com>
2024-03-01 14:54:54 +0800

    Create CustomStyleForBubbleChartVC.swift

---
+++ AAInfographicsDemo/Demo/AAChartModel/ViewController/CustomStyleForBubbleChartVC.swift
@@ -0,0 +1,30 @@
+//
+//  CustomStyleForBubbleChartVC.swift
+//  AAInfographicsDemo
+//
+//  Created by AnAn on 2024/3/1.
+//  Copyright © 2024 An An. All rights reserved.
+//
+
+import AAInfographics
+
+@available(iOS 10.0, macCatalyst 13.1, *)
+class CustomStyleForBubbleChartVC: AABaseChartVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    override func chartConfigurationWithSelectedChartTypeString(_ selectedChartTypeStr: String) -> Any? {
+        switch selectedChartTypeStr {
+        case "colorfulBarChart": return CustomStyleForBubbleChartComposer.configureNegativeColorMixedBubbleChart()
+        case "colorfulGradientColorBarChart": return CustomStyleForBubbleChartComposer.showAARadialGradientPositionAllEnumValuesWithBubbleChart()
+            
+        default: return nil
+        }
+    }
+
+}
