7247d1e813083dd561d33b039d25a5529968a873
An An <2236368544@qq.com>
2024-03-04 14:25:09 +0800

    Update CustomStyleForColumnChartVC.swift

--- AAInfographicsDemo/Demo/AAChartModel/ViewController/CustomStyleForColumnChartVC.swift
+++ AAInfographicsDemo/Demo/AAChartModel/ViewController/CustomStyleForColumnChartVC.swift
@@ -17,23 +17,23 @@ class CustomStyleForColumnChartVC: AABaseChartVC {
 
     override func chartConfigurationWithSelectedChartTypeString(_ selectedChartTypeStr: String) -> Any? {
         switch selectedChartTypeStr {
-        case "colorfulColumnChart":return CustomStyleForColumnChartComposer.colorfulColumnChart()
-        case "colorfulGradientColorColumnChart":return CustomStyleForColumnChartComposer.colorfulGradientColorColumnChart()
-        case "discontinuousDataColumnChart":return CustomStyleForColumnChartComposer.discontinuousDataColumnChart()
-        case "randomColorfulColumnChart":return CustomStyleForColumnChartComposer.randomColorfulColumnChart()
+        case "colorfulColumnChart": return CustomStyleForColumnChartComposer.colorfulColumnChart()
+        case "colorfulGradientColorColumnChart": return CustomStyleForColumnChartComposer.colorfulGradientColorColumnChart()
+        case "discontinuousDataColumnChart": return CustomStyleForColumnChartComposer.discontinuousDataColumnChart()
+        case "randomColorfulColumnChart": return CustomStyleForColumnChartComposer.randomColorfulColumnChart()
 
-        case "noneStackingPolarColumnChart":return CustomStyleForColumnChartComposer.noneStackingPolarColumnChart()
-        case "normalStackingPolarColumnChart":return CustomStyleForColumnChartComposer.normalStackingPolarColumnChart()
-        case "percentStackingPolarColumnChart":return CustomStyleForColumnChartComposer.percentStackingPolarColumnChart()
+        case "noneStackingPolarColumnChart": return CustomStyleForColumnChartComposer.noneStackingPolarColumnChart()
+        case "normalStackingPolarColumnChart": return CustomStyleForColumnChartComposer.normalStackingPolarColumnChart()
+        case "percentStackingPolarColumnChart": return CustomStyleForColumnChartComposer.percentStackingPolarColumnChart()
 
-        case "specialStyleForTheSingleDataElementOfColumnChart":return CustomStyleForColumnChartComposer.specialStyleForTheSingleDataElementOfColumnChart()
-        case "noMoreGroupingAndOverlapEachOtherColumnChart":return CustomStyleForColumnChartComposer.noMoreGroupingAndOverlapEachOtherColumnChart()
-        case "noMoreGroupingAndNestedColumnChart":return CustomStyleForColumnChartComposer.noMoreGroupingAndNestedColumnChart()
-        case "topRoundedCornersStackingColumnChart":return CustomStyleForColumnChartComposer.topRoundedCornersStackingColumnChart()
-        case "freeStyleRoundedCornersStackingColumnChart":return CustomStyleForColumnChartComposer.freeStyleRoundedCornersStackingColumnChart()
+        case "specialStyleForTheSingleDataElementOfColumnChart": return CustomStyleForColumnChartComposer.specialStyleForTheSingleDataElementOfColumnChart()
+        case "noMoreGroupingAndOverlapEachOtherColumnChart": return CustomStyleForColumnChartComposer.noMoreGroupingAndOverlapEachOtherColumnChart()
+        case "noMoreGroupingAndNestedColumnChart": return CustomStyleForColumnChartComposer.noMoreGroupingAndNestedColumnChart()
+        case "topRoundedCornersStackingColumnChart": return CustomStyleForColumnChartComposer.topRoundedCornersStackingColumnChart()
+        case "freeStyleRoundedCornersStackingColumnChart": return CustomStyleForColumnChartComposer.freeStyleRoundedCornersStackingColumnChart()
 
-        case "customBorderStyleAndStatesHoverColorColumnChart":return CustomStyleForColumnChartComposer.customBorderStyleAndStatesHoverColorColumnChart()
-        case "negativeDataMixedPositiveDataColumnChart":return CustomStyleForColumnChartComposer.negativeDataMixedPositiveDataColumnChart()
+        case "customBorderStyleAndStatesHoverColorColumnChart": return CustomStyleForColumnChartComposer.customBorderStyleAndStatesHoverColorColumnChart()
+        case "negativeDataMixedPositiveDataColumnChart": return CustomStyleForColumnChartComposer.negativeDataMixedPositiveDataColumnChart()
 
         default: return nil
         }
