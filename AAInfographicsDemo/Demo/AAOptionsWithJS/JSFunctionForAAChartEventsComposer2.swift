95211aa7e77b1944419231a61ea55fd57d4e5f3d
An An <2236368544@qq.com>
2024-08-02 19:14:22 +0800

    Fix https://github.com/AAChartModel/AAChartKit/issues/1557

--- AAInfographicsDemo/Demo/AAOptionsWithJS/JSFunctionForAAChartEventsComposer2.swift
+++ AAInfographicsDemo/Demo/AAOptionsWithJS/JSFunctionForAAChartEventsComposer2.swift
@@ -226,4 +226,94 @@ Highcharts.chart('container', {
                         .radius(10))
             ])
     }
+    
+    //https://github.com/AAChartModel/AAChartKit/issues/1557
+    //https://github.com/AAChartModel/AAChartCore/issues/199
+    //https://developer.mozilla.org/zh-CN/docs/Web/API/Touch_events/Using_Touch_Events
+    //https://developer.mozilla.org/zh-CN/docs/Web/API/MouseEvent
+    /*
+    在 JavaScript 中，与触摸事件对应的鼠标事件分别是：
+
+    - `touchstart` 对应 `mousedown`
+    - `touchend` 对应 `mouseup`
+
+    因此，将你的代码中的触摸事件改为鼠标事件后，代码可以改写为：
+
+     
+     1.
+    ```javascript
+    // 监听 mousedown 事件
+    container.addEventListener('mousedown', function() {
+      hideDataLabels(chart);
+    });
+
+    // 监听 mouseup 事件
+    container.addEventListener('mouseup', function() {
+      showDataLabels(chart);
+    });
+    ```
+
+    或者也可以改成为:
+     
+     2.
+     ```javascript
+     // 监听 mouseenter 事件
+     container.addEventListener('mouseenter', function() {
+       hideDataLabels(chart);
+     });
+
+     // 监听 mouseleave 事件
+     container.addEventListener('mouseleave', function() {
+       showDataLabels(chart);
+     });
+     ```
+    */
+    static func toggleDataLabelsOnTouch() -> AAOptions {
+        AAOptions()
+            .chart(AAChart()
+                .type(.areaspline)
+                .events(AAChartEvents()
+                    .load("""
+                    function() {
+                        const chart = this;
+                        const container = document.getElementById('container');
+
+                        container.addEventListener('touchstart', function() {
+                            hideDataLabels(chart);
+                        });
+
+                        container.addEventListener('touchend', function() {
+                            showDataLabels(chart);
+                        });
+
+                        function hideDataLabels(chart) {
+                            chart.series.forEach(function(series) {
+                                series.data.forEach(function(point) {
+                                    point.update({ dataLabels: { enabled: false } });
+                                });
+                            });
+                        }
+
+                        function showDataLabels(chart) {
+                            chart.series.forEach(function(series) {
+                                series.data.forEach(function(point) {
+                                    point.update({ dataLabels: { enabled: true } });
+                                });
+                            });
+                        }
+                    }
+                    """)))
+            .xAxis(AAXAxis()
+                .categories(["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"]))
+            .series([
+                AASeriesElement()
+                    .data([7.0, 6.9, 2.5, 14.5, 18.2, 21.5, 5.2, 26.5, 23.3, 45.3, 13.9, 9.6])
+                    .dataLabels(AADataLabels()
+                        .enabled(true))
+                    .marker(AAMarker()
+                        .lineColor(AAColor.red)
+                        .lineWidth(3)
+                        .radius(10))
+            ])
+    }
 }
