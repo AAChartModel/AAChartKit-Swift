d64160db18c1bc964d89f5eb21604e2e21185f22
An An <2236368544@qq.com>
2024-08-06 15:35:51 +0800

    Create CustomStyleForSplineChartComposer.swift

---
+++ AAInfographicsDemo/Demo/AAChartModel/Composer/CustomStyleForSplineChartComposer.swift
@@ -0,0 +1,41 @@
+//
+//  CustomStyleForSplineChartComposer.swift
+//  AAInfographicsDemo
+//
+//  Created by AnAn on 2024/8/6.
+//  Copyright © 2024 An An. All rights reserved.
+//
+
+import AAInfographics
+
+class CustomStyleForSpineChartComposer {
+    
+    static func mixedLineChart() -> AAChartModel { CustomStyleForLineChartComposer.mixedLineChart().chartType(.spline)}
+    static func stepLineChart() -> AAChartModel { CustomStyleForLineChartComposer.stepLineChart().chartType(.spline)}
+    static func stepAreaChart() -> AAChartModel { CustomStyleForLineChartComposer.stepAreaChart().chartType(.spline)}
+    static func customSingleDataLabelForLineChart() -> AAChartModel { CustomStyleForLineChartComposer.customSingleDataLabelForLineChart().chartType(.spline)}
+    static func shadowStyleLineChart() -> AAChartModel { CustomStyleForLineChartComposer.shadowStyleLineChart().chartType(.spline)}
+    
+    static func colorfulGradientLineChart() -> AAChartModel { CustomStyleForLineChartComposer.colorfulGradientLineChart().chartType(.spline)}
+    static func customMarkerSymbolContentLineChart() -> AAChartModel { CustomStyleForLineChartComposer.customMarkerSymbolContentLineChart().chartType(.spline)}
+    static func drawPointsWithCoordinatesForLineChart() -> AAChartModel { CustomStyleForLineChartComposer.drawPointsWithCoordinatesForLineChart().chartType(.spline)}
+    static func customHoverAndSelectHaloStyleForLineChart() -> AAChartModel { CustomStyleForLineChartComposer.customHoverAndSelectHaloStyleForLineChart().chartType(.spline)}
+    static func disableSomeOfLinesMouseTrackingEffectForLineChart() -> AAChartModel { CustomStyleForLineChartComposer.disableSomeOfLinesMouseTrackingEffectForLineChart().chartType(.spline)}
+
+    
+    static func colorfulShadowLineChart() -> AAChartModel { CustomStyleForLineChartComposer.colorfulShadowLineChart().chartType(.spline)}
+    static func colorfulDataLabelsStepLineChart() -> AAChartModel { CustomStyleForLineChartComposer.colorfulDataLabelsStepLineChart().chartType(.spline)}
+    static func disableMarkerHoverEffectForLineChart() -> AAChartModel { CustomStyleForLineChartComposer.disableMarkerHoverEffectForLineChart().chartType(.spline)}
+    static func maxAndMinDataLabelsForLineChart() -> AAChartModel { CustomStyleForLineChartComposer.maxAndMinDataLabelsForLineChart().chartType(.spline)}
+    static func dashStyleTypesMixedLineChart() -> AAChartModel { CustomStyleForLineChartComposer.dashStyleTypesMixedLineChart().chartType(.spline)}
+    
+    static func allLineDashStyleTypesMixedLineChart() -> AAChartModel { CustomStyleForLineChartComposer.allLineDashStyleTypesMixedLineChart().chartType(.spline)}
+    static func shadowLineChart() -> AAChartModel { CustomStyleForLineChartComposer.shadowLineChart().chartType(.spline)}
+    static func colorfulMarkersAndLinesLineChart() -> AAChartModel { CustomStyleForLineChartComposer.colorfulMarkersAndLinesLineChart().chartType(.spline)}
+    static func colorfulMarkersAndLinesLineChart2() -> AAChartModel { CustomStyleForLineChartComposer.colorfulMarkersAndLinesLineChart2().chartType(.spline)}
+    static func connectNullsForSingleAASeriesElementLineChart() -> AAChartModel { CustomStyleForLineChartComposer.connectNullsForSingleAASeriesElementLineChart().chartType(.spline)}
+
+    static func largeDifferencesInTheNumberOfDataInDifferentSeriesElementLineChart() -> AAChartModel { CustomStyleForLineChartComposer.largeDifferencesInTheNumberOfDataInDifferentSeriesElementLineChart().chartType(.spline)}
+    static func customDifferentDataLabelsShapeForLineChart() -> AAChartModel { CustomStyleForLineChartComposer.customDifferentDataLabelsShapeForLineChart().chartType(.spline)}
+    
+}
