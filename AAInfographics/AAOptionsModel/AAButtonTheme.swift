fa0562ccbc72498724a90ee3146919429982722c
An An <2236368544@qq.com>
2024-11-05 19:54:46 +0800

    Update AAButtonTheme.swift

--- AAInfographics/AAOptionsModel/AAButtonTheme.swift
+++ AAInfographics/AAOptionsModel/AAButtonTheme.swift
@@ -4,130 +4,31 @@
 //
 //  Created by AnAn on 2024/11/5.
 //  Copyright © 2024 An An. All rights reserved.
-//
+//*************** ...... SOURCE CODE ...... ***************
+//***...................................................***
+//*** https://github.com/AAChartModel/AAChartKit        ***
+//*** https://github.com/AAChartModel/AAChartKit-Swift  ***
+//***...................................................***
+//*************** ...... SOURCE CODE ...... ***************
 
-/**
- buttonTheme:{
- fill:#f7f7f7
- padding:8
- r:2
- states:{
- disabled:{
- style:{
- color:#cccccc
- }
- }
- hover:{
- fill:#e6e6e6
- }
- select:{
- fill:#e6e9ff
- style:{
- color:#000000
- fontWeight:bold
- }
- }
- }
- stroke:#cccccc
- stroke-width:1
- style:{
- color:#333333
- cursor:pointer
- fontSize:0.8em
- fontWeight:normal
- }
- }
+/*
+ 
+ * -------------------------------------------------------------------------------
+ *
+ *  🌕 🌖 🌗 🌘  ❀❀❀   WARM TIPS!!!   ❀❀❀ 🌑 🌒 🌓 🌔
+ *
+ * Please contact me on GitHub,if there are any problems encountered in use.
+ * GitHub Issues : https://github.com/AAChartModel/AAChartKit-Swift/issues
+ * -------------------------------------------------------------------------------
+ * And if you want to contribute for this project, please contact me as well
+ * GitHub        : https://github.com/AAChartModel
+ * StackOverflow : https://stackoverflow.com/users/12302132/codeforu
+ * JianShu       : https://www.jianshu.com/u/f1e6753d4254
+ * SegmentFault  : https://segmentfault.com/u/huanghunbieguan
+ *
+ * -------------------------------------------------------------------------------
+ 
  */
-public class AAButtonThemeStatesDisabled: AAObject {
-    public var style: AAStyle?
-    
-    @discardableResult
-    public func style(_ prop: AAStyle?) -> AAButtonThemeStatesDisabled {
-        style = prop
-        return self
-    }
-    
-    public override init() {
-        
-    }
-}
-
-public class AAButtonThemeStatesHover: AAObject {
-    public var fill: String?
-    public var style: AAStyle?
-    
-    @discardableResult
-    public func fill(_ prop: String?) -> AAButtonThemeStatesHover {
-        fill = prop
-        return self
-    }
-    
-    @discardableResult
-    public func style(_ prop: AAStyle?) -> AAButtonThemeStatesHover {
-        style = prop
-        return self
-    }
-    
-    public override init() {
-        
-    }
-}
-
-public class AAButtonThemeStatesSelect: AAObject {
-    public var fill: String?
-    public var style: AAStyle?
-    
-    @discardableResult
-    public func fill(_ prop: String?) -> AAButtonThemeStatesSelect {
-        fill = prop
-        return self
-    }
-    
-    @discardableResult
-    public func style(_ prop: AAStyle?) -> AAButtonThemeStatesSelect {
-        style = prop
-        return self
-    }
-    
-    public override init() {
-        
-    }
-}
-    
-
-
-
-
-    
-
-public class AAButtonThemeStates: AAObject {
-    public var disabled: AAButtonThemeStatesDisabled?
-    public var hover: AAButtonThemeStatesHover?
-    public var select: AAButtonThemeStatesSelect?
-    
-    @discardableResult
-    public func disabled(_ prop: AAButtonThemeStatesDisabled?) -> AAButtonThemeStates {
-        disabled = prop
-        return self
-    }
-    
-    @discardableResult
-    public func hover(_ prop: AAButtonThemeStatesHover?) -> AAButtonThemeStates {
-        hover = prop
-        return self
-    }
-    
-    @discardableResult
-    public func select(_ prop: AAButtonThemeStatesSelect?) -> AAButtonThemeStates {
-        select = prop
-        return self
-    }
-    
-    public override init() {
-
-    }
-}
-
 
 //https://api.highcharts.com/highcharts/global.buttonTheme
 public class AAButtonTheme: AAObject {
@@ -181,21 +82,6 @@ public class AAButtonTheme: AAObject {
         return self
     }
     
-    /*
-     public func toDictionaryProp() -> [String: Any]? {
-         var dic = [String: Any]()
-         if let fill = fill {
-             dic["fill"] = fill
-         }
-         if let stroke = stroke {
-             dic["stroke"] = stroke
-         }
-         if let strokeWidth = strokeWidth {
-             dic["stroke-width"] = strokeWidth
-         }
-         return dic
-     }
-     */
     public func toDictionaryProp() -> [String: Any]? {
         var dic = [String: Any]()
         if let fill = fill {
@@ -224,5 +110,91 @@ public class AAButtonTheme: AAObject {
     
     public override init() {
 
+    }
+}
+
+
+public class AAButtonThemeStates: AAObject {
+    public var disabled: AAButtonThemeStatesDisabled?
+    public var hover: AAButtonThemeStatesHover?
+    public var select: AAButtonThemeStatesSelect?
+    
+    @discardableResult
+    public func disabled(_ prop: AAButtonThemeStatesDisabled?) -> AAButtonThemeStates {
+        disabled = prop
+        return self
+    }
+    
+    @discardableResult
+    public func hover(_ prop: AAButtonThemeStatesHover?) -> AAButtonThemeStates {
+        hover = prop
+        return self
+    }
+    
+    @discardableResult
+    public func select(_ prop: AAButtonThemeStatesSelect?) -> AAButtonThemeStates {
+        select = prop
+        return self
+    }
+    
+    public override init() {
+
+    }
+}
+
+
+public class AAButtonThemeStatesDisabled: AAObject {
+    public var style: AAStyle?
+    
+    @discardableResult
+    public func style(_ prop: AAStyle?) -> AAButtonThemeStatesDisabled {
+        style = prop
+        return self
+    }
+    
+    public override init() {
+        
+    }
+}
+
+public class AAButtonThemeStatesHover: AAObject {
+    public var fill: String?
+    public var style: AAStyle?
+    
+    @discardableResult
+    public func fill(_ prop: String?) -> AAButtonThemeStatesHover {
+        fill = prop
+        return self
+    }
+    
+    @discardableResult
+    public func style(_ prop: AAStyle?) -> AAButtonThemeStatesHover {
+        style = prop
+        return self
+    }
+    
+    public override init() {
+        
+    }
+}
+
+public class AAButtonThemeStatesSelect: AAObject {
+    public var fill: String?
+    public var style: AAStyle?
+    
+    @discardableResult
+    public func fill(_ prop: String?) -> AAButtonThemeStatesSelect {
+        fill = prop
+        return self
+    }
+    
+    @discardableResult
+    public func style(_ prop: AAStyle?) -> AAButtonThemeStatesSelect {
+        style = prop
+        return self
+    }
+    
+    public override init() {
+        
     }
 }
