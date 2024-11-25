b40fdd198199aee122dff81f2b3a5da73f2b8e85
An An <2236368544@qq.com>
2024-11-25 09:37:11 +0800

    Update MixedChartComposer2.swift
    Add pieMixedLineMixedColumnChart

--- AAInfographicsDemo/Demo/AAChartModel/Composer/MixedChartComposer2.swift
+++ AAInfographicsDemo/Demo/AAChartModel/Composer/MixedChartComposer2.swift
@@ -199,4 +199,133 @@ class MixedChartComposer2: NSObject {
                         .radius(4))
             ])
     }
+    
+    /**
+     // Data retrieved from https://www.ssb.no/energi-og-industri/olje-og-gass/statistikk/sal-av-petroleumsprodukt/artikler/auka-sal-av-petroleumsprodukt-til-vegtrafikk
+     Highcharts.chart('container', {
+         title: {
+             text: 'Sales of petroleum products March, Norway',
+             align: 'left'
+         },
+         xAxis: {
+             categories: [
+                 'Jet fuel', 'Duty-free diesel', 'Petrol', 'Diesel', 'Gas oil'
+             ]
+         },
+         yAxis: {
+             title: {
+                 text: 'Million liters'
+             }
+         },
+         tooltip: {
+             valueSuffix: ' million liters'
+         },
+         plotOptions: {
+             series: {
+                 borderRadius: '25%'
+             }
+         },
+         series: [{
+             type: 'column',
+             name: '2020',
+             data: [59, 83, 65, 228, 184]
+         }, {
+             type: 'column',
+             name: '2021',
+             data: [24, 79, 72, 240, 167]
+         }, {
+             type: 'column',
+             name: '2022',
+             data: [58, 88, 75, 250, 176]
+         }, {
+             type: 'line',
+             step: 'center',
+             name: 'Average',
+             data: [47, 83.33, 70.66, 239.33, 175.66],
+             marker: {
+                 lineWidth: 2,
+                 lineColor: Highcharts.getOptions().colors[3],
+                 fillColor: 'white'
+             }
+         }, {
+             type: 'pie',
+             name: 'Total',
+             data: [{
+                 name: '2020',
+                 y: 619,
+                 color: Highcharts.getOptions().colors[0], // 2020 color
+                 dataLabels: {
+                     enabled: true,
+                     distance: -50,
+                     format: '{point.total} M',
+                     style: {
+                         fontSize: '15px'
+                     }
+                 }
+             }, {
+                 name: '2021',
+                 y: 586,
+                 color: Highcharts.getOptions().colors[1] // 2021 color
+             }, {
+                 name: '2022',
+                 y: 647,
+                 color: Highcharts.getOptions().colors[2] // 2022 color
+             }],
+             center: [75, 65],
+             size: 100,
+             innerSize: '70%',
+             showInLegend: false,
+             dataLabels: {
+                 enabled: false
+             }
+         }]
+     });
+     */
+    static func pieMixedLineMixedColumnChart() -> AAChartModel {
+        AAChartModel()
+            .title("Sales of petroleum products March, Norway")
+//            .titleAlign(.left)
+            .categories(["Jet fuel", "Duty-free diesel", "Petrol", "Diesel", "Gas oil"])
+            .yAxisTitle("Million liters")
+            .tooltipValueSuffix(" million liters")
+            .series([
+                AASeriesElement()
+                    .type(.column)
+                    .name("2020")
+                    .data([59, 83, 65, 228, 184]),
+                AASeriesElement()
+                    .type(.column)
+                    .name("2021")
+                    .data([24, 79, 72, 240, 167]),
+                AASeriesElement()
+                    .type(.column)
+                    .name("2022")
+                    .data([58, 88, 75, 250, 176]),
+                AASeriesElement()
+                    .type(.line)
+                    .step(.center)
+                    .name("Average")
+                    .data([47, 83.33, 70.66, 239.33, 175.66])
+                    .marker(AAMarker()
+                                .lineWidth(2)
+                                .lineColor(AAColor.white)
+                                .fillColor(AAColor.black)
+                    ),
+                AAPie()
+                    .type(.pie)
+//                    .name("Total")
+                    .data([
+                        ["2020", 619],
+                        ["2021", 586],
+                        ["2022", 647]
+                    ])
+                    .center([75, 65])
+                    .size(100)
+//                    .innerSize("70%")
+                    .showInLegend(false)
+                    .dataLabels(AADataLabels()
+                                    .enabled(false)
+                    )
+            ])
+    }
 }
