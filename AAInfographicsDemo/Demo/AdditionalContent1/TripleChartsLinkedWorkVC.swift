8c68251f6420d4f838043a15e72fb68d6a223ce9
An An <2236368544@qq.com>
2025-01-23 20:22:36 +0800

    Update TripleChartsLinkedWorkVC.swift

--- AAInfographicsDemo/Demo/AdditionalContent1/TripleChartsLinkedWorkVC.swift
+++ AAInfographicsDemo/Demo/AdditionalContent1/TripleChartsLinkedWorkVC.swift
@@ -6,125 +6,102 @@
 //  Copyright © 2024 An An. All rights reserved.
 //
 
+// ...existing code...
 import UIKit
 import AAInfographics
 
 class TripleChartsLinkedWorkVC: UIViewController {
-    var aaChartView1: AAChartView!
-    var aaChartView2: AAChartView!
-    var aaChartView3: AAChartView!
-    
+
+    private lazy var aaChartView1: AAChartView = {
+        let chartView = AAChartView()
+        chartView.isScrollEnabled = false
+        chartView.delegate = self
+        let aaOptions1 = AAOptions()
+            .chart(AAChart().type(.column))
+            .title(AATitle().text("Speed").align(.left))
+            .tooltip(AATooltip().shared(true))
+            .xAxis(AAXAxis()
+                .crosshair(AACrosshair()
+                    .color(AAColor.green)
+                    .width(2)
+                    .dashStyle(.longDashDot)
+                    .zIndex(5)))
+            .yAxis(AAYAxis().title(nil))
+            .series([JSFunctionBeforeAndAfterRenderingComposer3.configureSeriesArray()[0]])
+        chartView.aa_drawChartWithChartOptions(aaOptions1)
+        return chartView
+    }()
+
+    private lazy var aaChartView2: AAChartView = {
+        let chartView = AAChartView()
+        chartView.isScrollEnabled = false
+        chartView.delegate = self
+        let aaOptions2 = AAOptions()
+            .chart(AAChart().type(.column))
+            .title(AATitle().text("Some Title").align(.left))
+            .tooltip(AATooltip().shared(true))
+            .xAxis(AAXAxis()
+                .crosshair(AACrosshair()
+                    .color(AAColor.red)
+                    .width(2)
+                    .dashStyle(.longDashDot)
+                    .zIndex(5)))
+            .yAxis(AAYAxis().title(nil))
+            .series([JSFunctionBeforeAndAfterRenderingComposer3.configureSeriesArray()[1]])
+        chartView.aa_drawChartWithChartOptions(aaOptions2)
+        return chartView
+    }()
+
+    private lazy var aaChartView3: AAChartView = {
+        let chartView = AAChartView()
+        chartView.isScrollEnabled = false
+        chartView.delegate = self
+        let aaOptions3 = AAOptions()
+            .chart(AAChart().type(.column))
+            .title(AATitle().text("Another Title").align(.left))
+            .tooltip(AATooltip().shared(true))
+            .xAxis(AAXAxis()
+                .crosshair(AACrosshair()
+                    .color(AAColor.blue)
+                    .width(2)
+                    .dashStyle(.longDashDot)
+                    .zIndex(5)))
+            .yAxis(AAYAxis().title(nil))
+            .series([JSFunctionBeforeAndAfterRenderingComposer3.configureSeriesArray()[2]])
+        chartView.aa_drawChartWithChartOptions(aaOptions3)
+        return chartView
+    }()
+
+    private lazy var containerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 0
+        return stackView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Triple Charts Linked Work---3表联动"
         self.view.backgroundColor = .white
-        
-        setUpTheAAChartView1()
-        setUpTheAAChartView2()
-        setUpTheAAChartView3()
+        setupUIStackView()
     }
-    
-    // 配置第 1 个 AAChartView
-    func setUpTheAAChartView1() {
-        let chartViewWidth = self.view.frame.size.width
-        let screenHeight = self.view.frame.size.height
-        let frame = CGRect(x: 0, y: 60, width: chartViewWidth, height: screenHeight / 3)
-        let aaChartView1 = AAChartView(frame: frame)
-        aaChartView1.isScrollEnabled = false
-        self.view.addSubview(aaChartView1)
-        self.aaChartView1 = aaChartView1
-        self.aaChartView1.delegate = self
-        
-        let aaOptions1 = AAOptions()
-            .chart(AAChart()
-                .type(.column))
-            .title(AATitle()
-                .text("Speed")
-                .align(.left))
-            .tooltip(AATooltip()
-                .shared(true))
-            .xAxis(AAXAxis()
-                .crosshair(AACrosshair()
-                    .color(AAColor.green)
-                    .width(2)
-                    .dashStyle(.longDashDot)
-                    .zIndex(5)))
-            .yAxis(AAYAxis()
-                .title(nil))
-            .series([
-                JSFunctionBeforeAndAfterRenderingComposer3.configureSeriesArray()[0]
-            ])
-        
-        aaChartView1.aa_drawChartWithChartOptions(aaOptions1)
+
+    private func setupUIStackView() {
+        view.addSubview(containerStackView)
+        containerStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            containerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            containerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            containerStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        containerStackView.addArrangedSubview(aaChartView1)
+        containerStackView.addArrangedSubview(aaChartView2)
+        containerStackView.addArrangedSubview(aaChartView3)
     }
-    
-    // 配置第 2 个 AAChartView
-    func setUpTheAAChartView2() {
-        let chartViewWidth = self.view.frame.size.width
-        let screenHeight = self.view.frame.size.height
-        let frame = CGRect(x: 0, y: screenHeight / 3 + 60, width: chartViewWidth, height: screenHeight / 3 - 60)
-        let aaChartView2 = AAChartView(frame: frame)
-        aaChartView2.isScrollEnabled = false
-        self.view.addSubview(aaChartView2)
-        self.aaChartView2 = aaChartView2
-        self.aaChartView2.delegate = self
-        
-        let aaOptions2 = AAOptions()
-            .chart(AAChart()
-                .type(.column))
-            .title(AATitle()
-                .text("Elevation")
-                .align(.left))
-            .tooltip(AATooltip()
-                .shared(true))
-            .xAxis(AAXAxis()
-                .crosshair(AACrosshair()
-                    .color(AAColor.green)
-                    .width(2)
-                    .dashStyle(.longDashDot)
-                    .zIndex(5)))
-            .yAxis(AAYAxis()
-                .title(nil))
-            .series([
-                JSFunctionBeforeAndAfterRenderingComposer3.configureSeriesArray()[1]
-            ])
-        
-        aaChartView2.aa_drawChartWithChartOptions(aaOptions2)
-    }
-    
-    // 配置第 3 个 AAChartView
-    func setUpTheAAChartView3() {
-        let chartViewWidth = self.view.frame.size.width
-        let screenHeight = self.view.frame.size.height
-        let frame = CGRect(x: 0, y: (screenHeight / 3) * 2 + 60, width: chartViewWidth, height: screenHeight / 3 - 60)
-        let aaChartView3 = AAChartView(frame: frame)
-        aaChartView3.isScrollEnabled = false
-        self.view.addSubview(aaChartView3)
-        self.aaChartView3 = aaChartView3
-        self.aaChartView3.delegate = self
-        
-        let aaOptions3 = AAOptions()
-            .chart(AAChart()
-                .type(.column))
-            .title(AATitle()
-                .text("Heart Rate")
-                .align(.left))
-            .tooltip(AATooltip()
-                .shared(true))
-            .xAxis(AAXAxis()
-                .crosshair(AACrosshair()
-                    .color(AAColor.green)
-                    .width(2)
-                    .dashStyle(.longDashDot)
-                    .zIndex(5)))
-            .yAxis(AAYAxis()
-                .title(nil))
-            .series([
-                JSFunctionBeforeAndAfterRenderingComposer3.configureSeriesArray()[2]
-            ])
-        
-        aaChartView3.aa_drawChartWithChartOptions(aaOptions3)
-    }
+// ...existing code...
     
     
     func configureSyncRefreshTooltipJSString(message: AAMoveOverEventMessageModel) -> String {
