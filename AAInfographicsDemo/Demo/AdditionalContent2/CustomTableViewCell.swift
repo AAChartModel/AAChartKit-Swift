0b7e265c3e5ea6804da0f1795081debee0d5d8d5
zhaoyan.li <2236368544@qq.com>
2025-04-02 17:46:10 +0800

    Update CustomTableViewCell.swift

--- AAInfographicsDemo/Demo/AdditionalContent2/CustomTableViewCell.swift
+++ AAInfographicsDemo/Demo/AdditionalContent2/CustomTableViewCell.swift
@@ -28,17 +28,33 @@ class CustomTableViewCell: UITableViewCell {
     
     // 更新颜色以支持夜间模式
     func updateColors() {
-        backgroundColor = UIColor.systemBackground
-        contentView.backgroundColor = UIColor.systemBackground
-        titleLabel.textColor = UIColor.label
+        if #available(iOS 13.0, *) {
+            backgroundColor = UIColor.systemBackground
+        } else {
+            // Fallback on earlier versions
+        }
+        if #available(iOS 13.0, *) {
+            contentView.backgroundColor = UIColor.systemBackground
+        } else {
+            // Fallback on earlier versions
+        }
+        if #available(iOS 13.0, *) {
+            titleLabel.textColor = UIColor.label
+        } else {
+            // Fallback on earlier versions
+        }
         numberLabel.textColor = UIColor.white
     }
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         // 当用户界面样式发生变化时重新更新颜色
-        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
-            updateColors()
+        if #available(iOS 13.0, *) {
+            if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+                updateColors()
+            }
+        } else {
+            // Fallback on earlier versions
         }
     }
 }
